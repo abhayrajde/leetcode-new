b58bb7efb41f9f7736f3a5fc1724737008156f83
Abhay Rajde <abhaythakkar1234@gmail.com>
2021-12-08 22:36:06 +0000

    [LeetCode Sync] Runtime - 681 ms (5.01%), Memory - 25.6 MB (5.36%)

---
+++ my-folder/0053-maximum-subarray/solution.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        
+#         if(len(nums)==1):
+#             op = nums[0]
+#             return(op)
+#         else: 
+            
+        
+        max1 = nums[0]
+        sum1 = 0
+        
+        for i in nums:
+            sum1+=i
+            if(sum1>max1):
+                max1 = sum1
+            if(sum1 < 0):
+                sum1 = 0
+        return (max1)
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
