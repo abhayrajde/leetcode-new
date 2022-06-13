da718125c5538826b3d7259fe886084f23ebe9b8
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-06-13 19:50:53 +0000

    [LeetCode Sync] Runtime - 1628 ms (5.03%), Memory - 22.4 MB (9.32%)

--- my-folder/0121-best-time-to-buy-and-sell-stock/solution.py
+++ my-folder/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -35,7 +35,6 @@ class Solution(object):
             else:
                 return(0)
         '''
-        
         max_profit = 0
         buy = prices[0]
         for i in range(1,len(prices)):
@@ -51,7 +50,6 @@ class Solution(object):
                 buy = prices[i]
         
         return(max_profit)
-        
         """
         :type prices: List[int]
         :rtype: int
