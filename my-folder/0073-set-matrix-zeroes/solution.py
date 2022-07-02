dc764825bb5e6f65cd722f80252f4465f46e7e73
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-07-02 01:01:59 +0000

    [LeetCode Sync] Runtime - 111 ms (7.76%), Memory - 14.2 MB (6.08%)

--- my-folder/0073-set-matrix-zeroes/solution.py
+++ my-folder/0073-set-matrix-zeroes/solution.py
@@ -1,46 +1,31 @@
-import copy
 class Solution(object):
     def setZeroes(self, matrix):
-        
-        # RUNTIME ERROR (AS EXPECTED :)
-        listi = []
-        listj = []
-        matrix1 = copy.deepcopy(matrix)
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                if (matrix[i][j] == 0):
-                    listi.append(i)
-                    listj.append(j)
+        row, col = len(matrix), len(matrix[0])
+        rowzero = False
+        for r in range(row):
+            for c in range(col):
+                if(matrix[r][c] == 0):
+                    matrix[0][c] = 0
                     
-                    # a = i
-                    # b = j
-        for j in listj:
-            # if((j not in listj)): #and(i not in listi)):
-                # listj.append(j)
-            for x in range(len(matrix)):
-                matrix[x][j] = 0
-            # if(i not in listi):
-        for i in listi:
-            
-                #listi.append(i)
-            for y in range(len(matrix[0])):
-                matrix[i][y] = 0
-        """"""
-        """
-        if returning the different matrix was allowed then this could be one of the solution
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                if (matrix[i][j] == 0):
-                    # a = i
-                    # b = j
-                    for x in range(len(matrix)):
-                        matrix1[i][x] = 0
-                    for y in range(len(matrix[0])):
-                        matrix1[y][j] = 0
-        print(matrix1)
-    
-        """
-                        
+                    if(r == 0):
+                        rowzero = True
+                    else:
+                        matrix[r][0] = 0
+        
+        for r in range(1,row):
+            for c in range(1,col):
+                if((matrix[0][c] == 0) or (matrix[r][0] == 0)):
+                    matrix[r][c] = 0
+        
+        if(matrix[0][0] == 0):
+            for r in range(row):
+                matrix[r][0] = 0
+        if(rowzero):
+            for c in range(col):
+                matrix[0][c] = 0
+                
+                
+        
         """
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
