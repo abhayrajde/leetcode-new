e9dad588fe3747d8888c36df7f5d2db4f2df41b7
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-07-27 22:35:31 +0000

    [LeetCode Sync] Runtime - 17 ms (40.10%), Memory - 13.4 MB (7.29%)

--- my-folder/0198-house-robber/solution.py
+++ my-folder/0198-house-robber/solution.py
@@ -1,12 +1,13 @@
 class Solution(object):
     def rob(self, nums):
         dp = [0]*(len(nums)+1)
-        dp[0] = 0
+        # dp[0] = 0
         dp[1] = nums[0]
-        for i in range(1,len(nums)):
-            dp[i+1] = max(nums[i]+dp[i-1],dp[i])
-        return(dp[len(nums)])
+        
+        for i in range(2,len(dp)):
+            dp[i] = max((dp[i-2]+nums[i-1]),dp[i-1])
             
+        return(dp[len(nums)])
         """
         :type nums: List[int]
         :rtype: int
