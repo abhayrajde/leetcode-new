30a3bc6f5dc7a34e23e6975783b6140214f65dd2
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-08-04 20:29:20 +0000

    [LeetCode Sync] Runtime - 108 ms (14.07%), Memory - 18.1 MB (6.92%)

--- my-folder/0049-group-anagrams/solution.py
+++ my-folder/0049-group-anagrams/solution.py
@@ -1,18 +1,12 @@
 class Solution(object):
-    def groupAnagrams(self, strs):
-        # check if we can use trie for this question!!!!!!
-        
+    def groupAnagrams(self, strs):  
         hm = {}
         for i in range(len(strs)):
-            temp = sorted(strs[i])
-            # temp1 = temp[0:-1]
-            temp1 = ""
-            for j in range(len(temp)):
-                temp1+=temp[j]
-            if(temp1 in hm):
-                hm[temp1].append(strs[i])
+            temp = str(sorted(strs[i]))
+            if(temp in hm):
+                hm[temp].append(strs[i])
             else:
-                hm[temp1] = [strs[i]]
+                hm[temp] = [strs[i]]
         op = []
         for i in (hm.values()):
             op.append(i)
