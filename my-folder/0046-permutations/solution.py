260028e829618595c08145314b0e75edb7798457
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-08-10 06:41:45 +0000

    [LeetCode Sync] Runtime - 36 ms (5.27%), Memory - 13.7 MB (9.65%)

--- my-folder/0046-permutations/solution.py
+++ my-folder/0046-permutations/solution.py
@@ -1,32 +1,17 @@
 class Solution(object):
     def permute(self, nums):
         res = []
-        self.dfs(nums, [], res)
+        
+        def backtrack(dec_space, curr):
+            if(len(curr) == len(nums)):
+                res.append(curr)
+                return
+            
+            for i in range(len(dec_space)):
+                backtrack(dec_space[:i]+dec_space[i+1:], curr+[dec_space[i]])
+        backtrack(nums,[])
         return(res)
-        
-    def dfs(self, dec_space, curr, res):
-        if(not dec_space):
-            res.append(curr)
-            return
-        
-        for i in range(len(dec_space)):
-            self.dfs(dec_space[:i]+dec_space[i+1:], curr+[dec_space[i]], res)
-            
-        
-#         res = []
-        
-#         cur = []
-#         def dfs(i):
-#             if(i == len(nums)-1):
-#                 res.append(copy.copy(cur))
-#                 return
-            
-#             # if(i < len(nums)-1):
-#             cur.append(nums[i])
-#             dfs(i+1)
-#             cur.pop()
-        
-            
+                
         """
         :type nums: List[int]
         :rtype: List[List[int]]
