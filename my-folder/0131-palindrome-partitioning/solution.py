0124ecb58e21dca787a9566f18e4090826203f53
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-08-11 05:35:05 +0000

    [LeetCode Sync] Runtime - 1004 ms (5.07%), Memory - 33 MB (12.15%)

--- my-folder/0131-palindrome-partitioning/solution.py
+++ my-folder/0131-palindrome-partitioning/solution.py
@@ -1,22 +1,29 @@
 class Solution(object):
     def partition(self, s):
         res = []
-        self.dfs(s, [], res)
+        
+        curr = []
+        def ispal(s,i,j):
+            temp = s[i:j+1]
+            if(temp == temp[::-1]):
+                return(True)
+            return(False)
+        
+        def backtrack(i):
+            if i>=len(s):
+                res.append(curr[:])
+                return
+            
+            for j in range(i, len(s)):
+                if(ispal(s,i,j)):
+                    curr.append(s[i:j+1])
+                    backtrack(j+1)
+                    curr.pop()
+        backtrack(0)
         return(res)
+                    
+            
         
-        
-    def ispalindrome(self, s):
-        return(s == s[::-1])
-
-    def dfs(self, s, curr, res):
-        if(not s):
-            res.append(curr)
-            return
-
-        for i in range(len(s)):
-            if(self.ispalindrome(s[:i+1])):
-                self.dfs(s[i+1:], curr+[s[:i+1]], res)
-
         """
         :type s: str
         :rtype: List[List[str]]
