3a683f1e5582bd70a1dbcba67ddf7436692dc7df
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-08-20 22:37:06 +0000

    [LeetCode Sync] Runtime - 477 ms (18.86%), Memory - 32.4 MB (12.07%)

--- my-folder/0200-number-of-islands/solution.py
+++ my-folder/0200-number-of-islands/solution.py
@@ -1,66 +1,46 @@
 class Solution(object):
     def numIslands(self, grid):
-        if(not grid):
-            return(0)
-        
-        rows, cols = len(grid), len(grid[0])
-        islands = 0   # count of islands
-        visited = set()
-        
-        #Recursive DFS
-        def dfs(r,c):
-            directions = [[1,0],[-1,0],[0,1],[0,-1]]
-            print(r,c)
-            for row,col in directions:
-                dr,dc = row+r, col+c
-                if((dr>=0 and dr<rows) and (dc>=0 and dc<cols) and (dc in range(cols)) 
-                   and ((dr,dc) not in visited) and (grid[dr][dc] == "1")):
-                    visited.add((dr,dc))
-                    
-                    dfs(dr,dc)
-                # return
-            return
-        
-        for r in range(rows):
-            for c in range(cols):
-                if(grid[r][c] == "1" and ((r,c) not in visited)):
-                    # print("new")
-                    dfs(r,c)
-                    # print(r,c)
-                    islands+=1
-        return(islands)
-        
-        """
-        if(not grid):
-            return (0)
-        
-        rows,cols = len(grid), len(grid[0])
+        rows = len(grid)
+        cols = len(grid[0])
         visited = set()
         islands = 0
         
         def bfs(r,c):
             q = deque()
-            visited.add((r,c))
             q.append((r,c))
+            visited.add((r,c))
+            
             while q:
-                row, col = q.popleft()
-                directions = [[1,0],[-1,0],[0,1],[0,-1]]
-                for i,j in directions:
-                    r = row+i
-                    c = col+j
-                    if(r in range(rows) and
-                       c in range(cols) and
-                       grid[r][c] == "1" and 
-                       (r,c) not in visited):
-                        q.append((r,c))
-                        visited.add((r,c))
+                row,col = q.popleft()
+                #LEFT
+                if((row-1>=0) and (grid[row-1][col] == "1") and ((row-1,col) not in visited)):
+                    visited.add((row-1,col))
+                    q.append((row-1,col))
+                
+                #RIGHT
+                if((row+1<rows) and (grid[row+1][col] == "1") and ((row+1,col) not in visited)):
+                    visited.add((row+1,col))
+                    q.append((row+1,col))
+                
+                #UP
+                if((col-1>=0) and (grid[row][col-1] == "1") and ((row,col-1) not in visited)):
+                    visited.add((row,col-1))
+                    q.append((row,col-1))
+                    
+                #DOWN
+                if((col+1<cols) and (grid[row][col+1] == "1") and ((row,col+1) not in visited)):
+                    visited.add((row,col+1))
+                    q.append((row,col+1))
+                    
         for r in range(rows):
             for c in range(cols):
-                if(grid[r][c]=="1" and ((r,c) not in visited)):
+                if((grid[r][c] == "1") and ((r,c) not in visited)):
                     bfs(r,c)
                     islands+=1
-        return(islands)
-        """
+                    print(islands)
+        return islands
+                    
+                    
         """
         :type grid: List[List[str]]
         :rtype: int
