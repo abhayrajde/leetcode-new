ff924e557f36cb5ad3dc91394aa8a51c05ad7c06
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-08-23 20:46:12 +0000

    [LeetCode Sync] Runtime - 58 ms (5.07%), Memory - 13.4 MB (7.73%)

--- my-folder/1036-rotting-oranges/solution.py
+++ my-folder/1036-rotting-oranges/solution.py
@@ -1,57 +1,53 @@
 class Solution(object):
     def orangesRotting(self, grid):
+        visited = set()
         q = deque()
-        
-        rows, cols = len(grid), len(grid[0])
+        rows = len(grid)
+        cols = len(grid[0])
         fresh = 0
+        time = 0
         for r in range(rows):
             for c in range(cols):
-                if(grid[r][c] == 2):
+                if (grid[r][c] == 2):
                     q.append((r,c))
                 if(grid[r][c] == 1):
-                    fresh+=1
+                    fresh += 1
         
-        visited = set()
-        mins = 0
-        while(q and fresh > 0):
-            print(q)
-            for i in range(len(q)):
+        while(q and fresh>0):
+            for i in range(len(q)):    
                 r,c = q.popleft()
-                
-                # Down
-                if(r+1 < rows and ((r+1,c) not in visited) and (grid[r+1][c]==1)):
-                    q.append((r+1,c))
-                    visited.add((r+1,c))
-                    grid[r+1][c] = 2
-                    fresh-=1
-                
-                # Up
-                if(r-1 >= 0 and ((r-1,c) not in visited) and (grid[r-1][c]==1)):
-                    q.append((r-1,c))
+
+                #UP
+                if(r-1>=0 and ((r-1,c) not in visited) and (grid[r-1][c] == 1)):
                     visited.add((r-1,c))
-                    grid[r-1][c] = 2
+                    q.append((r-1,c))
                     fresh-=1
-                
-                # Left
-                if(c-1 >= 0 and ((r,c-1) not in visited) and (grid[r][c-1]==1)):
-                    q.append((r,c-1))
+                    grid[r-1][c] == 2
+                    
+                #DOWN
+                if(r+1<rows and ((r+1,c) not in visited) and (grid[r+1][c] == 1)):
+                    visited.add((r+1,c))
+                    q.append((r+1,c))
+                    fresh-=1
+                    grid[r+1][c] == 2
+                    
+                #LEFT
+                if(c-1>=0 and ((r,c-1) not in visited) and (grid[r][c-1] == 1)):
                     visited.add((r,c-1))
-                    grid[r][c-1] = 2
+                    q.append((r,c-1))
                     fresh-=1
-                
-                # Right
-                if(c+1 < cols and ((r,c+1) not in visited) and (grid[r][c+1]==1)):
-                    q.append((r,c+1))
+                    grid[r][c-1] == 2
+                    
+                #RIGHT
+                if(c+1<cols and ((r,c+1) not in visited) and (grid[r][c+1] == 1)):
                     visited.add((r,c+1))
-                    grid[r][c+1] = 2
-                    fresh -= 1
-            mins+=1
+                    q.append((r,c+1))
+                    fresh-=1
+                    grid[r][c+1] == 2
+            time+=1
         if(fresh == 0):
-            return(mins)
-        return(-1)
-                
-                
-                
+            return time
+        return (-1)        
         """
         :type grid: List[List[int]]
         :rtype: int
