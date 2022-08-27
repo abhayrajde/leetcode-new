a23b8ff0dce8113b31358765927bf014ad936d9b
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-08-27 04:52:12 +0000

    [LeetCode Sync] Runtime - 115 ms (6.98%), Memory - 16.5 MB (6.07%)

--- my-folder/0207-course-schedule/solution.py
+++ my-folder/0207-course-schedule/solution.py
@@ -1,31 +1,31 @@
 class Solution(object):
     def canFinish(self, numCourses, prerequisites):
-        
         prereq = {c:[] for c in range(numCourses)}
-        
-        for crs, pre in prerequisites:
+        for crs,pre in prerequisites:
             prereq[crs].append(pre)
-        
+            
         visited = set()
         cycle = set()
+        
         def dfs(crs):
-            if(crs in cycle):
+            if crs in cycle:
                 return False
-            if(crs in visited):
+            if crs in visited:
                 return True
             
             cycle.add(crs)
             for pre in prereq[crs]:
-                if(dfs(pre)==False):
+                if(dfs(pre) == False):
                     return False
-            cycle.remove(crs)
             visited.add(crs)
-            return True
-        
+            cycle.remove(crs)
+            
         for crs in range(numCourses):
             if(dfs(crs) == False):
                 return False
         return True
+                
+        
         """
         :type numCourses: int
         :type prerequisites: List[List[int]]
