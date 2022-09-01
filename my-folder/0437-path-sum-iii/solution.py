f15f8eb4f388ef541a8e428807c98fb7c288fe74
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-09-01 19:10:33 +0000

    [LeetCode Sync] Runtime - 63 ms (57.08%), Memory - 14.9 MB (8.17%)

--- my-folder/0437-path-sum-iii/solution.py
+++ my-folder/0437-path-sum-iii/solution.py
@@ -6,28 +6,33 @@
 #         self.right = right
 class Solution(object):
     def pathSum(self, root, targetSum):
-        hm = defaultdict(int)
-        hm[0]=1
-        self.count = 0
-        def dfs(node, currsum,hm):
-            # exit condition
-            if not node:
-                return 0
-
-            currsum += node.val
-
-            self.count += hm.get((currsum-targetSum),0)
-
-            hm[currsum] += 1
-
-            dfs(node.left,currsum,hm)
-            dfs(node.right,currsum,hm)
-            
-            hm[currsum] -= 1
-        dfs(root,0,hm)
-        return(self.count)
-
+        prevsum = collections.defaultdict(int)
+        prevsum[0] = 1
         
+        res = [0]
+        def dfs(node,currsum):
+            
+            if not node:
+                return 0 
+            
+            currsum += node.val
+            
+            if (currsum - targetSum) in prevsum and prevsum[currsum-targetSum]>0:
+                res[0]+=prevsum.get((currsum-targetSum),0)
+                
+            prevsum[currsum]+=1
+            
+            dfs(node.left, currsum)
+            # prevsum[currsum]-=1
+            # currsum-=node.val
+            
+            dfs(node.right, currsum)
+            
+            prevsum[currsum]-=1
+            currsum-=node.val
+            
+        dfs(root, 0)
+        return res[0]
         """
         :type root: TreeNode
         :type targetSum: int
