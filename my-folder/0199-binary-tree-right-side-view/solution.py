a948ea94ee4fa7199cb94d6584d665d177e0b6a9
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-09-03 05:53:11 +0000

    [LeetCode Sync] Runtime - 14 ms (73.20%), Memory - 13.6 MB (13.98%)

--- my-folder/0199-binary-tree-right-side-view/solution.py
+++ my-folder/0199-binary-tree-right-side-view/solution.py
@@ -1,3 +1,4 @@
+from collections import deque
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,25 +7,27 @@
 #         self.right = right
 class Solution(object):
     def rightSideView(self, root):
-        if not root:
-            return []
+        if not root: return []
         
         q = deque([root])
+        
         res = []
-        while q:
-            qlen = len(q)-1
-            for i in range(len(q)):
+        while(q):
+            len_q = len(q)
+            for i in range(len_q):
                 curr = q.popleft()
                 
-                # Logic to add the right most element in result
-                if(i == qlen):
+                if(i == len_q-1):
                     res.append(curr.val)
-                    
+                
                 if(curr.left):
                     q.append(curr.left)
+                
                 if(curr.right):
                     q.append(curr.right)
-        return(res)
+        return res
+                
+            
         """
         :type root: TreeNode
         :rtype: List[int]
