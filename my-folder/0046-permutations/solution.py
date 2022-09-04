278fded1e9e220a1f0e1d51ca2f8e1d5e376bf05
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-09-04 21:01:48 +0000

    [LeetCode Sync] Runtime - 58 ms (5.27%), Memory - 13.8 MB (9.65%)

--- my-folder/0046-permutations/solution.py
+++ my-folder/0046-permutations/solution.py
@@ -1,17 +1,38 @@
 class Solution(object):
-    def permute(self, nums):
+    def permute(self, nums): 
         res = []
         
-        def backtrack(dec_space, curr):
-            if(len(curr) == len(nums)):
-                res.append(curr)
+        def backtrack(ind,nums):
+            if(ind == len(nums)):
+                res.append(nums[:])
                 return
             
-            for i in range(len(dec_space)):
-                backtrack(dec_space[:i]+dec_space[i+1:], curr+[dec_space[i]])
-        backtrack(nums,[])
-        return(res)
+            for i in range(ind,len(nums)):
+                nums[ind],nums[i] = nums[i],nums[ind]
+                backtrack(ind+1,nums)
+                nums[ind],nums[i] = nums[i],nums[ind]
+        backtrack(0,nums)
+        return res
                 
+        
+        #Space Complexity  - O(N!) * N
+#         res = []
+#         freq = [0]*len(nums)
+#         curr = []
+#         def backtrack(nums,curr,res,freq):
+#             if (len(curr) == len(nums)):
+#                 res.append(curr[:])
+#                 return
+            
+#             for i in range(len(nums)):
+#                 if(freq[i] == 0):
+#                     freq[i] = 1
+#                     curr.append(nums[i])
+#                     backtrack(nums,curr,res,freq)
+#                     curr.pop()
+#                     freq[i] = 0
+#         backtrack(nums,curr,res,freq)
+#         return res
         """
         :type nums: List[int]
         :rtype: List[List[int]]
