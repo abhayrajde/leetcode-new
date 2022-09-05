aefcf3983cbb661d1ffcb7099fc0392b3ee94d54
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-09-05 16:07:43 +0000

    [LeetCode Sync] Runtime - 31 ms (8.08%), Memory - 13.5 MB (5.58%)

--- my-folder/0017-letter-combinations-of-a-phone-number/solution.py
+++ my-folder/0017-letter-combinations-of-a-phone-number/solution.py
@@ -3,23 +3,23 @@ class Solution(object):
         if not digits:
             return []
         hm = {'2':"abc", '3':"def", '4':"ghi", '5':"jkl", '6':"mno", '7':"pqrs", '8':"tuv", '9':"wxyz"}
-        
         res = []
-        
-        curr = ""
-        
-        def backtrack(digits,curr):
-            if not digits:
-                res.append(curr)
+        self.curr = ""
+        #23
+        def backtrack(nums):
+            if not nums:
+                res.append(self.curr[:])
                 return
             
-            for i in hm[digits[0]]:
-                backtrack(digits[1:],curr+i)
-            
-        backtrack(digits,curr)
-        return(res)
-                
+            for j in hm[nums[0]]:
+                self.curr+=j
         
+                #some backtraking business performed here
+                backtrack(nums[1:])
+                
+                self.curr = self.curr[:-1]
+        backtrack(digits)
+        return(res)
         """
         :type digits: str
         :rtype: List[str]
