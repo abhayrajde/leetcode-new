102876018d31b3c8da24f796119f2277a616d1a2
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-10-17 06:16:56 +0000

    [LeetCode Sync] Runtime - 72 ms (5.85%), Memory - 14.3 MB (8.99%)

--- my-folder/0138-copy-list-with-random-pointer/solution.py
+++ my-folder/0138-copy-list-with-random-pointer/solution.py
@@ -9,20 +9,43 @@ class Node:
 
 class Solution(object):
     def copyRandomList(self, head):
-        oldtocopy = {None:None}
+        oldtocopy = { None:None }
+
         curr = head
+
+        # STEP1: WE WILL MAP THE COPY NODE WITH ACTUAL NODE
         while curr:
             copy = Node(curr.val)
             oldtocopy[curr] = copy
+            # copy.next = curr.next
             curr = curr.next
         
+        # STEP2: GET THE COPY NODE FROM THE MAP & ADD NEXT AND RANDOM POINTER SAME AS ACTUAL NODE
         curr = head
         while curr:
             copy = oldtocopy[curr]
             copy.next = oldtocopy[curr.next]
             copy.random = oldtocopy[curr.random]
             curr = curr.next
+        
         return oldtocopy[head]
+        
+
+        
+#         oldtocopy = {None:None}
+#         curr = head
+#         while curr:
+#             copy = Node(curr.val)
+#             oldtocopy[curr] = copy
+#             curr = curr.next
+        
+#         curr = head
+#         while curr:
+#             copy = oldtocopy[curr]
+#             copy.next = oldtocopy[curr.next]
+#             copy.random = oldtocopy[curr.random]
+#             curr = curr.next
+#         return oldtocopy[head]
         """
         :type head: Node
         :rtype: Node
