4f8a9a48e4f4bb6f67a49755614fc49cafc8db77
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-10-17 19:51:22 +0000

    [LeetCode Sync] Runtime - 7863 ms (11.32%), Memory - 13.6 MB (13.37%)

--- my-folder/0079-word-search/solution.py
+++ my-folder/0079-word-search/solution.py
@@ -1,32 +1,34 @@
 class Solution(object):
     def exist(self, board, word):
-        # res = False
-        
+
         rows = len(board)
         cols = len(board[0])
         
         visited = set()
         
+        #r,c is for position in the board matrix and i is for letter position in word
         def backtrack(r,c,i):
-            if(i == len(word)):
+            if i == len(word):
                 return True
             
-            if(r<0 or c<0 or r>=rows or c>=cols or (r,c) in visited or board[r][c]!=word[i]):
+            if(r < 0 or r >= rows or c < 0 or c >= cols or (r,c) in visited or board[r][c] != word[i]):
                 return False
             
             visited.add((r,c))
             
-            res = (backtrack(r-1,c,i+1) or backtrack(r+1,c,i+1) or backtrack(r,c-1,i+1) or backtrack(r,c+1,i+1))
+            #TODO - business logic
+            # check left, up, right, down elements i board
+            res = (backtrack(r,c-1,i+1) or backtrack(r-1,c,i+1) or backtrack(r,c+1,i+1) or backtrack(r+1,c,i+1))
             
             visited.remove((r,c))
             return res
+        
         for r in range(rows):
             for c in range(cols):
                 if board[r][c] == word[0] and backtrack(r,c,0):
                     return True
         return False
-    
-                    
+
         """
         :type board: List[List[str]]
         :type word: str
