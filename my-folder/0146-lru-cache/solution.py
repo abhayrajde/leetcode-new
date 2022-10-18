1938bc28fba7874e1685ef8bbdb3584bcc82437a
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-10-18 01:10:03 +0000

    [LeetCode Sync] Runtime - 868 ms (25.60%), Memory - 80.1 MB (5.92%)

--- my-folder/0146-lru-cache/solution.py
+++ my-folder/0146-lru-cache/solution.py
@@ -1,44 +1,48 @@
-class Node(object):
+class Node:
     def __init__(self,key,val):
         self.key = key
         self.val = val
-        self.prev = self.next = None
-        
-        self.key = key
-        self.val = val
-        self.prev = self.next = None
-        
+        self.prev = None
+        self.next = None
+
 class LRUCache(object):
+
     def __init__(self, capacity):
         self.cap = capacity
-        self.cache = {}
-        self.left = Node(0,0)
-        self.right = Node(0,0)
+        self.cache = {} # map key to node
+        
+        #Left - will help to find least recently used Node
+        #Right - will help to find the most recent
+        self.left, self.right = Node(0,0), Node(0,0)
         self.left.next = self.right
         self.right.prev = self.left
+        
         """
         :type capacity: int
         """
-        
-
+    
+    # remove node from the list
     def remove(self, node):
-        prev = node.prev
-        nxt = node.next
+        prev, nxt = node.prev, node.next
         prev.next = nxt
         nxt.prev = prev
         
+    # insert node in the list at the right
     def insert(self,node):
-        node.next = self.right
-        node.prev = self.right.prev
-        self.right.prev.next = node
-        self.right.prev = node
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt, prev
         
+    
     def get(self, key):
-        if(key in self.cache):
+        if key in self.cache:
+            #TODO: update the most recent 
             self.remove(self.cache[key])
             self.insert(self.cache[key])
             return self.cache[key].val
-        return -1    
+        
+        return -1
+        
         """
         :type key: int
         :rtype: int
@@ -46,15 +50,17 @@ class LRUCache(object):
         
 
     def put(self, key, value):
-        if (key in self.cache):
+        if key in self.cache:
             self.remove(self.cache[key])
         self.cache[key] = Node(key,value)
         self.insert(self.cache[key])
         
-        if(len(self.cache)>self.cap):
+        if len(self.cache) > self.cap:
+            # remove from the list and delete thr LRU from the hashmap
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
+         
         """
         :type key: int
         :type value: int
