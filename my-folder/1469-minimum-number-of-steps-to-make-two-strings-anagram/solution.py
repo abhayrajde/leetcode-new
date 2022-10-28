c3b0d729b335bc1ee4b695a6b57601fca950308a
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-10-28 07:22:09 +0000

    [LeetCode Sync] Runtime - 386 ms (10.81%), Memory - 14.3 MB (9.46%)

---
+++ my-folder/1469-minimum-number-of-steps-to-make-two-strings-anagram/solution.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def minSteps(self, s, t):
+        dict_t = defaultdict(int)
+        
+        for i in t:
+            dict_t[i] += 1
+            
+        count = 0
+        
+        for i in s:
+            if i not in dict_t or dict_t[i] == 0:
+                count+=1
+            else:
+                dict_t[i] -=1
+        return count
+            
+        
+        """
+        :type s: str
+        :type t: str
+        :rtype: int
+        """
+        
