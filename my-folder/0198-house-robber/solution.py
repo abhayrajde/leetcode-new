dca14e73da9bff74c222032950db1e3c4e61c165
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-11-10 02:28:26 +0000

    [LeetCode Sync] Runtime - 38 ms (5.23%), Memory - 13.6 MB (7.29%)

--- my-folder/0198-house-robber/solution.py
+++ my-folder/0198-house-robber/solution.py
@@ -1,13 +1,48 @@
 class Solution(object):
+    
+    #Tabulation Technique - Dynamic Programming
     def rob(self, nums):
-        dp = [0]*(len(nums)+1)
-        # dp[0] = 0
-        dp[1] = nums[0]
         
-        for i in range(2,len(dp)):
-            dp[i] = max((dp[i-2]+nums[i-1]),dp[i-1])
+        dp = [0]*len(nums)
+        
+        for i in range(len(nums)):
             
-        return(dp[len(nums)])
+            pick = nums[i]
+            if i>1:
+                pick += dp[i-2]
+                
+            not_pick = 0
+            if i > 0:
+                not_pick += dp[i-1]
+            
+            dp[i] = max(pick, not_pick)
+            
+        return dp[-1]
+                
+    
+    # Memoization Technique - Dynamic Programming
+    def rob(self, nums):
+
+        dp = [-1]*len(nums)
+
+
+        def func(ind):
+            if ind >= len(nums):
+                return 0
+
+            if dp[ind] != -1:
+                return dp[ind]
+
+            pick = nums[ind] + func(ind+2)
+
+            not_pick = 0 + func(ind + 1)
+
+            dp[ind] = max(pick, not_pick)
+
+            return dp[ind]
+        res = func(0)
+        return res
+
         """
         :type nums: List[int]
         :rtype: int
