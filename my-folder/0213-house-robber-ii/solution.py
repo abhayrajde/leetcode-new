7f5d1b1bd6eaeb4e4117eb6e526790248873b3cc
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-11-10 02:28:17 +0000

    [LeetCode Sync] Runtime - 28 ms (5.32%), Memory - 13.5 MB (7.22%)

--- my-folder/0213-house-robber-ii/solution.py
+++ my-folder/0213-house-robber-ii/solution.py
@@ -1,24 +1,58 @@
 class Solution(object):
+    
+    # Tabulation Technique - Dynamic Programming
     def rob(self, nums):
+        if len(nums) == 1:
+            return nums[0]
         
-        # if the number of houses is 1 or 2 or 3 then may the richest house be robbed
-        if(len(nums)==1 or len(nums)==2 or len(nums)==3):
-            return(max(nums))
+        def dp_tab(nums):
+            dp = [0]*len(nums)
+
+            for i in range(len(nums)):
+                pick = nums[i]
+
+                if i>1:
+                    pick += dp[i-2]
+                not_pick = 0
+                if i > 0:
+                    not_pick += dp[i-1]
+
+                dp[i] = max(pick,not_pick)
+
+            return(dp[-1])
+        keep_first = dp_tab(nums[:-1])
+        keep_last = dp_tab(nums[1:])
+        return max(keep_first,keep_last)
+    
+    # Memoization Technique - Dynamic Programming
+    def rob(self, nums):
+        if len(nums) == 1:
+            return nums[0]
         
-        #logic for more then 3 houses
-        else:
-            dp1 = [0]*len(nums)
-            dp2 = [0]*len(nums)
-            dp1[0],dp2[0] = 0,0
-            dp1[1],dp2[1] = nums[0],nums[1]
-            for i in range(2,len(dp1)):
-                dp1[i] = max(dp1[i-1],dp1[i-2]+nums[i-1])
-                
-            for i in range(2,len(dp2)):
-                dp2[i] = max(dp2[i-1], dp2[i-2]+nums[i])
-            
-            return(max(dp1[len(nums)-1],dp2[len(nums)-1]))
-            
+        def dp_mem(nums):
+            dp = [-1]*len(nums)
+
+
+            def func(ind):
+                if ind >= len(nums):
+                    return 0
+
+                if dp[ind] != -1:
+                    return dp[ind]
+
+                pick = nums[ind] + func(ind+2)
+
+                not_pick = 0 + func(ind + 1)
+
+                dp[ind] = max(pick, not_pick)
+
+                return dp[ind]
+            res = func(0)
+            return res
+        
+        keep_first = dp_mem(nums[:-1])
+        keep_last = dp_mem(nums[1:])
+        return max(keep_first,keep_last)
         """
         :type nums: List[int]
         :rtype: int
