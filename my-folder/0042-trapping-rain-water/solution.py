9813b90dea72d6369922dade1e23c801acb63b53
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-11-20 22:58:24 +0000

    [LeetCode Sync] Runtime - 96 ms (20.50%), Memory - 15 MB (5.03%)

--- my-folder/0042-trapping-rain-water/solution.py
+++ my-folder/0042-trapping-rain-water/solution.py
@@ -1,40 +1,22 @@
 class Solution(object):
     def trap(self, height):
-        maxleft = []
-        maxl = height[0]
-        maxleft.append(maxl)
-        for i in range(1,len(height)):
-            if(height[i]>maxl):
-                maxl = height[i]
-                maxleft.append(maxl)
-            else:
-                maxleft.append(maxl)
+        l, r = 0, len(height)-1
+        leftmax = height[l]
+        rightmax = height[r]
+        res = 0 
         
-        maxright = []
-        maxr = height[-1]
-        maxright.append(maxr)
-        for i in range(len(height)-2,-1,-1):
-            if(height[i]>maxr):
-                maxr = height[i]
-                maxright.append(maxr)
-            else:
-                maxright.append(maxr)
-        maxright.reverse()
-        
-        minlr = []
-        for i in range(len(height)):
-            minlr.append(min(maxright[i],maxleft[i]))
-        
-        count = 0
-        for i in range(len(height)):
-            temp = minlr[i] - height[i]
-            if(temp > 0):
-                count += temp
-        
-        return(count)
+        while l < r:
+            if leftmax < rightmax:
+                l += 1
+                leftmax = max(leftmax, height[l])
+                res += leftmax - height[l]
             
-            # start = height[i]
-            
+            else:
+                r -= 1
+                rightmax = max(rightmax, height[r])
+                res += rightmax -height[r]
+        return res
+        
         """
         :type height: List[int]
         :rtype: int
