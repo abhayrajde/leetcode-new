98b09ccc552d3bbe2ca59d089bbd915b52bb5d7a
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-12-04 19:30:26 +0000

    [LeetCode Sync] Runtime - 24 ms (23.64%), Memory - 13.8 MB (14.11%)

--- my-folder/0090-subsets-ii/solution.py
+++ my-folder/0090-subsets-ii/solution.py
@@ -1,24 +1,22 @@
 class Solution(object):
     def subsetsWithDup(self, nums):
+        # Base Condition
+        if not nums:
+            return []
+        
+        nums= sorted(nums)
         res = []
-        nums.sort()
-        subset = []
-        def backtrack(ind):
-            if(ind >= len(nums)):
-                res.append(subset[:])
+        def dfs(i, curr):
+            if i >= len(nums):
+                res.append(curr[:])
                 return
-            subset.append(nums[ind])
-            backtrack(ind+1)
             
-            # while(ind != 0 and nums[ind] == nums[ind-1]):
-            while(ind < len(nums) - 1 and nums[ind] == nums[ind + 1]):
-                ind+=1
-            
-            subset.pop()
-            backtrack(ind+1)
-        backtrack(0)
+            pick = dfs(i+1, curr+[nums[i]])
+            while(i < len(nums) - 1 and nums[i] == nums[i + 1]):
+                i = i+1
+            not_pick = dfs(i+1, curr)
+        dfs(0,[])
         return res
-            
         """
         :type nums: List[int]
         :rtype: List[List[int]]
