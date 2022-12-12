04d1e59a8f415081449068bacbabfa4eacb5e721
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-12-12 07:00:11 +0000

    [LeetCode Sync] Runtime - 138 ms (63.83%), Memory - 17.6 MB (13.83%)

--- my-folder/0127-word-ladder/solution.py
+++ my-folder/0127-word-ladder/solution.py
@@ -1,34 +1,39 @@
+from collections import defaultdict
+from collections import deque
 class Solution(object):
     def ladderLength(self, beginWord, endWord, wordList):
+        #Base Case
+        if endWord not in wordList:
+            return 0
         
-        nei = {}
+        nei = defaultdict(list)
         wordList.append(beginWord)
-        
         for word in wordList:
             for j in range(len(word)):
                 pattern = word[:j] + "*" + word[j+1:]
-                if(pattern not in nei):
-                    nei[pattern] = []
                 nei[pattern].append(word)
-                
-                
+        
         visited = set([beginWord])
         q = deque([beginWord])
         res = 1
-        while(q):
+        
+        while q:
             for i in range(len(q)):
                 word = q.popleft()
-                if(word == endWord):
+                if word == endWord:
                     return res
                 for j in range(len(word)):
-                    pattern = word[:j] + "*" + word[j+1:]
+                    pattern = word[:j] + "*" + word[j+1:] 
                     for neiword in nei[pattern]:
-                        if(neiword not in visited):
+                        if neiword not in visited:
                             visited.add(neiword)
                             q.append(neiword)
+                             
             res+=1
-        return 0
-                    
+        
+        return 0 
+                
+        
         """
         :type beginWord: str
         :type endWord: str
