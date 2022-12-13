ab457e67ae61b1b4d4222e63ddd8262d726bf8a5
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-12-13 12:38:44 +0000

    [LeetCode Sync] Runtime - 35 ms (86.25%), Memory - 14.7 MB (12.94%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,6 +1,8 @@
 class Solution(object):
     def twoSum(self, nums, target):
         
+        #Time C - O(N)
+        #Space C - O(N)
         hm = {}
         
         for i in range(len(nums)):
@@ -8,6 +10,20 @@ class Solution(object):
                 return(i,hm[target-nums[i]])
             else:
                 hm[nums[i]] = i
+        
+        # we have to return the index, so sorting not allowed.
+        
+#         nums = sorted(nums)
+        
+#         l = 0
+#         r = len(nums) - 1
+#         while l < r:
+#             if nums[l] + nums[r] == target:
+#                 return([l,r])
+#             if nums[l] + nums[r] < target:
+#                 l += 1
+#             else:
+#                 r -= 1
         
         """
         :type nums: List[int]
