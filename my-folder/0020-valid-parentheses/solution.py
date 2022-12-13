d79a25d1c23ad67dc9ce929c02293e7ebc629c2c
Abhay Rajde <abhaythakkar1234@gmail.com>
2022-12-13 07:16:55 +0000

    [LeetCode Sync] Runtime - 22 ms (12.01%), Memory - 13.8 MB (17.05%)

--- my-folder/0020-valid-parentheses/solution.py
+++ my-folder/0020-valid-parentheses/solution.py
@@ -1,38 +1,61 @@
-class Solution(object):
+# create dict where key are the closing brackets and there values are opening brackets
+    # if we encounter the closing bracket and at the same time
+    # if the stack is empty or the last element in the stack is not dict[curr_ele]
+        #then return False
+class Solution:
+    # @return a boolean
     def isValid(self, s):
         stack = []
-        for i in s:
-            if(i == "(" or i == "{" or i == "["):
-                stack.insert(0,i)
-            elif(i == ")"):
-                if(stack == []):
-                    return(False)
-                elif(stack[0] == "("):
-                    stack.pop(0)
+        dict1 = {"]":"[", "}":"{", ")":"("}
+        for c in s:
+            if c in dict1:
+                if not stack or stack[-1] != dict1[c]:
+                    return False
+                    
                 else:
-                    return(False)
-            elif(i == "}"):
-                if(stack == []):
-                    return(False)
-                elif(stack[0] == "{"):
-                    stack.pop(0)
-                else:
-                    return(False)
-            elif(i == "]"):
-                if(stack == []):
-                    return(False)
-                elif(stack[0] == "["):
-                    stack.pop(0)
-                else:
-                    return(False)
-        if(stack == []):
-            return(True)
-        else:
-            return(False)
+                    stack.pop()
+            else:
+                stack.append(c)
+                
+        return (True) if not stack else False
+        
+    
+    
+# class Solution(object):
+#     def isValid(self, s):
+#         stack = []
+#         for i in s:
+#             if(i == "(" or i == "{" or i == "["):
+#                 stack.insert(0,i)
+#             elif(i == ")"):
+#                 if(stack == []):
+#                     return(False)
+#                 elif(stack[0] == "("):
+#                     stack.pop(0)
+#                 else:
+#                     return(False)
+#             elif(i == "}"):
+#                 if(stack == []):
+#                     return(False)
+#                 elif(stack[0] == "{"):
+#                     stack.pop(0)
+#                 else:
+#                     return(False)
+#             elif(i == "]"):
+#                 if(stack == []):
+#                     return(False)
+#                 elif(stack[0] == "["):
+#                     stack.pop(0)
+#                 else:
+#                     return(False)
+#         if(stack == []):
+#             return(True)
+#         else:
+#             return(False)
             
                 
-        """
-        :type s: str
-        :rtype: bool
-        """
+#         """
+#         :type s: str
+#         :rtype: bool
+#         """
         
