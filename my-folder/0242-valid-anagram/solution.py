6bfb4ef9411aaf0c5b7fb7dbc96af0977c731498
Abhay Rajde <abhaythakkar1234@gmail.com>
2023-08-21 23:21:16 +0000

    [LeetCode Sync] Runtime - 26 ms (86.39%), Memory - 15.7 MB (5.23%)

--- my-folder/0242-valid-anagram/solution.py
+++ my-folder/0242-valid-anagram/solution.py
@@ -1,38 +1,38 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        #Using HashMap Logic
         
-        if(len(s)!=len(t)):
-            return(False)
+        if(len(s) != len(t)):
+            return False
         
         hms = {}
         hmt = {}
+        
+        #elements count for string s
         for i in range(len(s)):
-            if(s[i] in hms):
-                hms[s[i]]+=1
-            else:
+            if(s[i] not in hms):
                 hms[s[i]] = 1
-        
-        for i in range(len(t)):
-            if(t[i] in hmt):
-                hmt[t[i]]+=1
             else:
-                hmt[t[i]] = 1
+                hms[s[i]] += 1
         
+        #elements count for string t
+        for i in range(len(t)):
+            if(t[i] not in hmt):
+                hmt[t[i]] = 1
+            else:
+                hmt[t[i]] += 1
+        
+        #compare both the hashmap dictionary
+        # if(len(t)>len(s)):
+        #     s,t = t,s
+        #     hms,hmt = hmt,hms
+            
         for i in range(len(s)):
             if((s[i] not in hmt) or (hms[s[i]] != hmt[s[i]])):
-                return(False)
-        return(True)
+                return False
+        return True
+    
         
         
-        
-        #Using normal Sorting Technique - Time Complexity = 2nlogn +2n | Space = O(1)
-        # s = sorted(s)
-        # t = sorted(t)
-        # if(s == t):
-        #     return(True)
-        # else:
-        #     return(False)
         """
         :type s: str
         :type t: str
