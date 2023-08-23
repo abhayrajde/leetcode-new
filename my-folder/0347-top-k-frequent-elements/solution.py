77f84111536bc3e2720d40c00e6c4c37dc3a7521
Abhay Rajde <abhaythakkar1234@gmail.com>
2023-08-23 01:53:03 +0000

    [LeetCode Sync] Runtime - 71 ms (75.10%), Memory - 16.6 MB (32.01%)

--- my-folder/0347-top-k-frequent-elements/solution.py
+++ my-folder/0347-top-k-frequent-elements/solution.py
@@ -1,16 +1,16 @@
 class Solution(object):
     def topKFrequent(self, nums, k):
-        hm = {}
-        for i in range(len(nums)):
-            if(nums[i] in hm):
-                hm[nums[i]] += 1
-            else:
-                hm[nums[i]] = 1
+        hm = defaultdict(int)
+        
+        for i in nums:
+            hm[i]+=1
+        
         hm = sorted(hm.items(), key = lambda x:x[1], reverse = True)
-        op = []
+        result = []
         for i in range(k):
-            op.append(hm[i][0])
-        return(op)
+            result.append(hm[i][0])
+        return result
+            
         """
         :type nums: List[int]
         :type k: int
