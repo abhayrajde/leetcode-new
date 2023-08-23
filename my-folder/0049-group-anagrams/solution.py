b05ff9adad12cfbf5277a6b7e3c420c6d3766a0b
Abhay Rajde <abhaythakkar1234@gmail.com>
2023-08-23 01:48:08 +0000

    [LeetCode Sync] Runtime - 77 ms (39.38%), Memory - 17.7 MB (27.45%)

--- my-folder/0049-group-anagrams/solution.py
+++ my-folder/0049-group-anagrams/solution.py
@@ -1,32 +1,17 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        res = defaultdict(list)
         
-        for s in strs:
-            count = [0]*26
-            
-            for c in s:
-                count[ord(c) - ord("a")]+=1
-            
-            res[tuple(count)].append(s)
-            
-        return res.values()
-        
-#         from collections import defaultdict
-#         hm = defaultdict(list)
-        
-#         hm = {}
-#         for i in range(len(strs)):
-#             word = str(sorted(strs[i]))
-#        #     if(word not in hm):
-#         #        hm[word] = []
-#             hm[word].append(strs[i])
-#         # return list(hm.values())    
-#         res = []
-#         for lists in hm.values():
-#             res.append(lists)
-#         return res
-            
+        hm = {}
+        for i in range(len(strs)):
+            temp = str(sorted(strs[i]))
+            if(temp in hm):
+                hm[temp].append(strs[i])
+            else:
+                hm[temp] = [strs[i]]
+        op = []
+        for i in (hm.values()):
+            op.append(i)
+        return(op)
         """
         :type strs: List[str]
         :rtype: List[List[str]]
