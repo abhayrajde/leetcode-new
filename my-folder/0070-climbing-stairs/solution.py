778c76eff8196b2e0f5ff7f921f8981d538c40be
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-04-28 16:12:01 +0000

    [LeetCode Sync] Runtime - 11 ms (77.09%), Memory - 11.6 MB (58.66%)

--- my-folder/0070-climbing-stairs/solution.py
+++ my-folder/0070-climbing-stairs/solution.py
@@ -1,21 +1,31 @@
 class Solution(object):
     def climbStairs(self, n):
-        if(n == 1):
-            return(1)
-        dp = [0]*(n+1)
-        dp[0] = 1
-        dp[1] = 1
-        for i in range(2,n+1):
-            dp[i] = dp[i-1]+dp[i-2]
-        return(dp[n])
-        # one = 1
-        # two = 1
-        # for i in range (n-1):
-        #     temp = one
-        #     one = one+two
-        #     two = temp
-        # return(one)
-            
+        dp = [-1]*(n+1)
+        # Dp: Tabulation - Bottom Up Approach
+        def dptab(n):
+            dp[0] = dp[1] = 1
+            for i in range(2, n+1):
+                dp[i] = dp[i-1] + dp[i-2]
+            return(dp[n])
+
+        # DP: Memoization - Top Down Approach
+        def dpmem(n):
+            if(n <= 1):
+                return 1
+
+            if(dp[n] != -1):
+                return dp[n]
+
+            dp[n] = dpmem(n-1) + dpmem(n-2)
+            return(dp[n])
+
+        # Recursion solution
+        def recursion(n):
+            if(n <= 1):
+                return 1
+            return(recursion(n-1)+recursion(n-2))
+
+        return(dptab(n))
         """
         :type n: int
         :rtype: int
