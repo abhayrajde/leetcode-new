f3ffa74a2b953421f5e4c5f351f806fbf032f57d
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-06-19 16:42:11 +0000

    [LeetCode Sync] Runtime - 765 ms (32.37%), Memory - 0B (100.00%)

---
+++ my-folder/1161-project-employees-i/solution.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT p.project_id, ROUND(SUM(experience_years)/COUNT(p.employee_id),2) AS average_years
+FROM
+Project p
+LEFT JOIN 
+Employee e
+ON 
+p.employee_id = e.employee_id
+GROUP BY p.project_id
