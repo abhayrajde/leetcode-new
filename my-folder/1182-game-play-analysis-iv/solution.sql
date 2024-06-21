9176f90eb78359f66feb25f28b4a8eb9e0188695
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-06-21 20:37:01 +0000

    [LeetCode Sync] Runtime - 512 ms (84.45%), Memory - 0B (100.00%)

---
+++ my-folder/1182-game-play-analysis-iv/solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT ROUND(COUNT(DISTINCT b.player_id)/COUNT(DISTINCT a.player_id),2) AS fraction FROM 
+(SELECT player_id,MIN(event_date) AS event_date FROM Activity GROUP BY player_id) a 
+LEFT JOIN activity b
+ON a.player_id = b.player_id 
+AND b.event_date = DATE_ADD(a.event_date, INTERVAL 1 DAY)
+
+
