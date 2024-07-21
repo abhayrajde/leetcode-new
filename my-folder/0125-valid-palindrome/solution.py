a431bbeb99560b0adcd5d8d2cd45fb489a5a26b6
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-07-21 00:12:42 +0000

    [LeetCode Sync] Runtime - 300 ms (15.84%), Memory - 13 MB (59.19%)

--- my-folder/0125-valid-palindrome/solution.py
+++ my-folder/0125-valid-palindrome/solution.py
@@ -1,17 +1,17 @@
 class Solution(object):
     def isPalindrome(self, s):
-        s = s.lower()
         str1 = ""
         for i in s:
-            if(i.isalnum()):
-                str1+=i
-        i,j = 0,len(str1)-1
-        while(i<j):
-            if(str1[i]!=str1[j]):
-                return(False)
-            i+=1
-            j-=1
-        return(True)
+            if i.isalnum():
+                str1 += i.lower()
+        i = 0
+        j = len(str1) - 1
+        while(i < j):
+            if(str1[i] != str1[j]):
+                return False
+            i += 1
+            j -= 1
+        return True
         """
         :type s: str
         :rtype: bool
