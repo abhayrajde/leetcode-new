f686a6b6d1967c06d52503f41d71d292e3cdd6c9
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-08-22 23:25:00 +0000

    [LeetCode Sync] Runtime - 13 ms (1.41%), Memory - 11.5 MB (100.00%)

---
+++ my-folder/0476-number-complement/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findComplement(self, num):
+        bit = 0
+        todo = num
+        while todo:
+            bit = bit << 1
+            bit = bit ^ 1
+            todo = todo >> 1
+        return bit ^ num
+        """
+        :type num: int
+        :rtype: int
+        """
+        
