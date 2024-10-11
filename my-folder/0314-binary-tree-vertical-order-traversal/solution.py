19809dd62219a6d3f1f1b5d3aa43cf04a5d9ffa7
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-11 04:40:12 +0000

    [LeetCode Sync] Runtime - 17 ms (5.56%), Memory - 11.8 MB (100.00%)

--- my-folder/0314-binary-tree-vertical-order-traversal/solution.py
+++ my-folder/0314-binary-tree-vertical-order-traversal/solution.py
@@ -1,38 +1,35 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if root == None: return []
-        if root.left == None and root.right == None:
-            return [[root.val]]
-        def traverse(node, track, level, vLevel):
-            if node.left == None and node.right == None:
-                if level in track:
-                    track[level].append((node.val, vLevel))
-                else:
-                    track[level] = [(node.val, vLevel)]
-                return track
-            if node.left:
-                track = traverse(node.left, track, level - 1, vLevel + 1)
-            if level in track:
-                track[level].append((node.val, vLevel))
-            else:
-                track[level] = [(node.val, vLevel)]
-            if node.right:
-                track = traverse(node.right, track, level + 1, vLevel + 1)
-            return track
-        track = {}
-        traverse(root, track, 0, 0)
-        output = []
-        for key in sorted(dict(track.items())):
-            value = track[key]
-            output.append([x[0] for x in sorted(value, key=lambda x: x[1])])
-        return output
-        
+class Solution(object):
+    def verticalOrder(self, root):
+        dict1 = defaultdict(list)
+        h_dist, v_dist = 0, 0
+
+        def dfs(node, h_dist, v_dist):
+            if not node:
+                return
+            dict1[h_dist].append((node.val, v_dist))
+
+            left = dfs(node.left, h_dist - 1, v_dist + 1)
+            right = dfs(node.right, h_dist + 1, v_dist + 1)
             
-        
+        dfs(root,0,0)
+        pairs = sorted(dict1.items())
+
+        result = []
+        for key, values in pairs:
+            sortedValues = sorted(values, key = lambda x:x[1])
+            temp = []
+            for i in sortedValues:
+                temp.append(i[0])
+            result.append(temp)
+        return result
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
         
