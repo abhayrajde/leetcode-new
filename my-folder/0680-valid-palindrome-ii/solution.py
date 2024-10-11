f74a4f721ff911cc898676f1db012b69ab74b92d
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-11 00:25:00 +0000

    [LeetCode Sync] Runtime - 73 ms (9.48%), Memory - 17.1 MB (100.00%)

--- my-folder/0680-valid-palindrome-ii/solution.py
+++ my-folder/0680-valid-palindrome-ii/solution.py
@@ -1,15 +1,12 @@
-class Solution(object):
-    def validPalindrome(self, s):
-        l,r = 0, len(s) - 1
-        
-        while l < r:
-            if s[l] != s[r]:
-                skipl,skipr = s[l + 1:r + 1], s[l:r]
-                return skipl == skipl[::-1] or skipr == skipr[::-1]
-            l, r = l + 1, r - 1
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1
+        while i <= j:
+            if s[i] != s[j]:
+                s1 = s[:i] + s[i+1:]
+                s2 = s[:j] + s[j+1:]
+                return s1==s1[::-1] or s2==s2[::-1]
+            i += 1
+            j -= 1
         return True
-        """
-        :type s: str
-        :rtype: bool
-        """
-        
