64938005586c191955292195b15b641369311efe
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-12 23:05:32 +0000

    [LeetCode Sync] Runtime - 34 ms (0.83%), Memory - 16.7 MB (100.00%)

--- my-folder/0050-powx-n/solution.py
+++ my-folder/0050-powx-n/solution.py
@@ -1,3 +1,9 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return float(x**n)
+        if n == 0:
+            return 1
+        if n == -1:
+            return 1/x
+        if n == 1:
+            return x
+        return self.myPow(x*x, n//2) * self.myPow(x, n%2)
