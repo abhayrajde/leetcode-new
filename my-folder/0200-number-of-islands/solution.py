846d0127719e5f675a6dd1004bd177d2552e60c6
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-12 14:57:10 +0000

    [LeetCode Sync] Runtime - 258 ms (20.74%), Memory - 33.5 MB (7.49%)

--- my-folder/0200-number-of-islands/solution.py
+++ my-folder/0200-number-of-islands/solution.py
@@ -1,46 +1,34 @@
 class Solution(object):
     def numIslands(self, grid):
-        rows = len(grid)
-        cols = len(grid[0])
+        if not grid:
+            return 0
         visited = set()
         islands = 0
-        
-        def bfs(r,c):
-            q = deque()
-            q.append((r,c))
-            visited.add((r,c))
-            
-            while q:
-                row,col = q.popleft()
-                #LEFT
-                if((row-1>=0) and (grid[row-1][col] == "1") and ((row-1,col) not in visited)):
-                    visited.add((row-1,col))
-                    q.append((row-1,col))
-                
-                #RIGHT
-                if((row+1<rows) and (grid[row+1][col] == "1") and ((row+1,col) not in visited)):
-                    visited.add((row+1,col))
-                    q.append((row+1,col))
-                
-                #UP
-                if((col-1>=0) and (grid[row][col-1] == "1") and ((row,col-1) not in visited)):
-                    visited.add((row,col-1))
-                    q.append((row,col-1))
-                    
-                #DOWN
-                if((col+1<cols) and (grid[row][col+1] == "1") and ((row,col+1) not in visited)):
-                    visited.add((row,col+1))
-                    q.append((row,col+1))
-                    
-        for r in range(rows):
-            for c in range(cols):
-                if((grid[r][c] == "1") and ((r,c) not in visited)):
-                    bfs(r,c)
-                    islands+=1
-                    print(islands)
+        ROWS = len(grid)
+        COLS = len(grid[0])
+
+
+        def dfs(r,c):
+            while(r >= 0 and r < ROWS and c >= 0 and c < COLS and 
+                (r,c) not in visited and grid[r][c] == "1"):
+                visited.add((r,c))
+                dfs(r+1,c)
+                dfs(r-1,c)
+                dfs(r,c+1)
+                dfs(r,c-1)
+
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if(grid[r][c] == "1" and (r,c) not in visited):
+                    dfs(r,c)
+                    islands += 1
         return islands
+
+
                     
                     
+
         """
         :type grid: List[List[str]]
         :rtype: int
