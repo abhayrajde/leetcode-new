2145107f0f54239d64ae97cb6bb07f7f9f631f9c
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-12 19:04:36 +0000

    [LeetCode Sync] Runtime - 98 ms (6.83%), Memory - 14.8 MB (67.27%)

--- my-folder/0695-max-area-of-island/solution.py
+++ my-folder/0695-max-area-of-island/solution.py
@@ -1,26 +1,29 @@
 class Solution(object):
     def maxAreaOfIsland(self, grid):
-        rows = len(grid)
-        cols = len(grid[0])
-        
+        ROWS = len(grid)
+        COLS = len(grid[0])
         visited = set()
-        maxarea = 0
+        maxArea = [0]
+
+        if not grid:
+            return 0
         
-        def dfs(r, c):
-            if( r < 0 or c < 0 or
-              r == rows or c == cols or
-              (r,c) in visited or grid[r][c] == 0):
-                return 0
-            visited.add((r,c))
-            return(1 + dfs(r+1,c) +
-                        dfs(r-1,c) +
-                        dfs(r,c+1) +
-                        dfs(r,c-1))
-            
-        for r in range(rows):
-            for c in range(cols):
-                maxarea = max(maxarea, dfs(r,c))
-        return(maxarea)
+        def dfs(r,c,area):
+            while(r >= 0 and r < ROWS and c>= 0 and c < COLS 
+                and (r,c) not in visited and grid[r][c] == 1):
+                visited.add((r,c))
+                area = (dfs(r+1,c,area+1) + dfs(r-1,c,area+1) + dfs(r,c+1,area+1) + dfs(r,c-1,area+1))
+                return area + 1
+            return 0
+                # maxArea[0] = max(maxArea[0],area)
+
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if(grid[r][c] == 1 and (r,c) not in visited):
+                    area = dfs(r,c,1)
+                    maxArea[0] = max(maxArea[0], area)
+        return maxArea[0]
         """
         :type grid: List[List[int]]
         :rtype: int
