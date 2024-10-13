839e333c4458713d2e09d8b2bbd86aa59cf25d4a
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-13 21:42:49 +0000

    [LeetCode Sync] Runtime - 38 ms (95.91%), Memory - 17.5 MB (100.00%)

--- my-folder/0138-copy-list-with-random-pointer/solution.py
+++ my-folder/0138-copy-list-with-random-pointer/solution.py
@@ -1,53 +1,26 @@
 """
 # Definition for a Node.
 class Node:
-    def __init__(self, x, next=None, random=None):
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
         self.val = int(x)
         self.next = next
         self.random = random
 """
 
-class Solution(object):
-    def copyRandomList(self, head):
-        oldtocopy = { None:None }
-
-        curr = head
-
-        # STEP1: WE WILL MAP THE COPY NODE WITH ACTUAL NODE
-        while curr:
-            copy = Node(curr.val)
-            oldtocopy[curr] = copy
-            # copy.next = curr.next
-            curr = curr.next
-        
-        # STEP2: GET THE COPY NODE FROM THE MAP & ADD NEXT AND RANDOM POINTER SAME AS ACTUAL NODE
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head: return None
+        oldToNew = {}
         curr = head
         while curr:
-            copy = oldtocopy[curr]
-            copy.next = oldtocopy[curr.next]
-            copy.random = oldtocopy[curr.random]
+            oldToNew[curr] = Node(curr.val)
+            curr = curr.next
+
+        curr = head
+        while curr:
+            oldToNew[curr].next = oldToNew.get(curr.next)
+            oldToNew[curr].random = oldToNew.get(curr.random)
             curr = curr.next
         
-        return oldtocopy[head]
-        
+        return oldToNew[head]
 
-        
-#         oldtocopy = {None:None}
-#         curr = head
-#         while curr:
-#             copy = Node(curr.val)
-#             oldtocopy[curr] = copy
-#             curr = curr.next
-        
-#         curr = head
-#         while curr:
-#             copy = oldtocopy[curr]
-#             copy.next = oldtocopy[curr.next]
-#             copy.random = oldtocopy[curr.random]
-#             curr = curr.next
-#         return oldtocopy[head]
-        """
-        :type head: Node
-        :rtype: Node
-        """
-        
