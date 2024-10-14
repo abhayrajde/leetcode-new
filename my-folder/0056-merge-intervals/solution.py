09727af43688e57e44fba5bb745e52488dc6ade6
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-14 01:22:16 +0000

    [LeetCode Sync] Runtime - 123 ms (7.16%), Memory - 20.7 MB (100.00%)

--- my-folder/0056-merge-intervals/solution.py
+++ my-folder/0056-merge-intervals/solution.py
@@ -1,22 +1,15 @@
-class Solution(object):
-    def merge(self, intervals):
-        
-        intervals = sorted(intervals, key = lambda x: x[0])
-        i = 0
-        while(i < len(intervals)-1):
-            if intervals[i][1] >= intervals[i+1][0]:
-                
-                intervals[i] = [intervals[i][0], max(intervals[i][1], intervals[i+1][1])]
-                intervals.remove(intervals[i+1])
-            
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        output = []
+        intervals.sort(key=lambda x: x[0])
+
+        prev = intervals[0]
+        for i in range(1, len(intervals)):
+            interval = intervals[i]
+            if interval[0] <= prev[1]:
+                prev[1] = max(prev[1], interval[1])
             else:
-                i+=1
-            
-        return intervals
-                
-        
-        """
-        :type intervals: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        
+                output.append(prev)
+                prev = interval
+        output.append(prev)
+        return output
