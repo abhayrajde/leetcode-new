a689f80ee486350b3763538bb94d9512ba906e5a
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-15 22:37:00 +0000

    [LeetCode Sync] Runtime - 210 ms (7.88%), Memory - 13.3 MB (99.57%)

--- my-folder/0417-pacific-atlantic-water-flow/solution.py
+++ my-folder/0417-pacific-atlantic-water-flow/solution.py
@@ -1,37 +1,37 @@
 class Solution(object):
     def pacificAtlantic(self, heights):
+        rows = len(heights)
+        cols = len(heights[0])
+
         pac = set()
         atl = set()
-        
-        row,col = len(heights),len(heights[0])
-        
-        def dfs(r,c,visited,prevheight):
-            if((r,c) in visited or
-                r < 0 or c < 0 or
-                r==row or c == col or
-                heights[r][c] < prevheight):
+
+        def dfs(r,c, visited, prevHeight):
+            if (r < 0 or r == rows or c < 0 or c == cols
+                or (r,c) in visited or heights[r][c] < prevHeight):
                 return
             visited.add((r,c))
-            dfs(r+1,c,visited,heights[r][c])
-            dfs(r-1,c,visited,heights[r][c])
-            dfs(r,c+1,visited,heights[r][c])
-            dfs(r,c-1,visited,heights[r][c])
+            dfs(r + 1,c,visited,heights[r][c])
+            dfs(r - 1,c,visited,heights[r][c])
+            dfs(r,c + 1,visited,heights[r][c])
+            dfs(r,c - 1,visited,heights[r][c])
+
+        for c in range(cols):
+            dfs(0,c,pac,-1)
+            dfs(rows-1,c,atl,-1)
         
-        for c in range(col):
-            dfs(0,c,pac,heights[0][c])
-            dfs(row-1,c,atl,heights[row-1][c])
-        
-        for r in range(row):
-            dfs(r,0,pac,heights[r][0])
-            dfs(r,col-1,atl,heights[r][col-1])
+        for r in range(rows):
+            dfs(r,0,pac,-1)
+            dfs(r,cols-1,atl,-1)
         
         res = []
-        for r in range(row):
-            for c in range(col):
-                if(((r,c) in pac) and ((r,c) in atl)):
-                    res.append((r,c))
-        return(res)
-                    
+        for r in range(rows):
+            for c in range(cols):
+                if (r,c) in pac and (r,c) in atl:
+                    res.append([r,c])
+        return res
+
+        
         """
         :type heights: List[List[int]]
         :rtype: List[List[int]]
