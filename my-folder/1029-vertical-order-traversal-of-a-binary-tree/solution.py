826d5d36b425f192368fec88d550ca0529684250
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-16 00:32:24 +0000

    [LeetCode Sync] Runtime - 34 ms (0.68%), Memory - 16.8 MB (100.00%)

--- my-folder/1029-vertical-order-traversal-of-a-binary-tree/solution.py
+++ my-folder/1029-vertical-order-traversal-of-a-binary-tree/solution.py
@@ -1,39 +1,29 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def verticalTraversal(self, root):
-        if not root: return
-        v_dist = 0
-        h_dist = 0
-        values = defaultdict(list)
-        res = []
+class Solution:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
         
-        def verticalorder(node, h_dist, v_dist, values):
+        def traverse(node, r, c):
+            nonlocal cache
             if not node:
                 return
-            
-            values[h_dist].append((v_dist,node.val))
-            
-            verticalorder(node.left, h_dist-1, v_dist+1, values)
-            verticalorder(node.right, h_dist+1, v_dist+1, values)
-        
-        verticalorder(root,h_dist, v_dist, values)
-        
-        for x in sorted(values.keys()):
-            column = []
-            for i in sorted(values[x]):
-                column.append(i[1])
-            res.append(column)
-        
-        return res
-            
-            
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
-        
+            if c in cache:
+                cache[c].append((r, node.val))
+            else:
+                cache[c] = [(r, node.val)]
+            traverse(node.left, r+1, c-1)
+            traverse(node.right, r+1, c+1)
+
+        cache = {}
+        traverse(root, 0, 0)
+        output = []
+        for c in sorted(cache.keys()):
+            sec = []
+            for a, b in sorted(cache[c], key=lambda x:(x[0], x[1])):
+                sec.append(b)
+            output.append(sec)
+        return output
