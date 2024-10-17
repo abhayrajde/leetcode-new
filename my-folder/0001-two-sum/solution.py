50cbc3570aaedcfe599d536f6f2723ccbabb01a8
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-17 00:45:27 +0000

    [LeetCode Sync] Runtime - 39 ms (32.26%), Memory - 18.1 MB (100.00%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,32 +1,9 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        
-        #Time C - O(N)
-        #Space C - O(N)
-        hm = {}
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        cache = {}
+    
         for i in range(len(nums)):
-            if (target - nums[i] in hm):
-                return(i,hm[target - nums[i]])
-            else:
-                hm[nums[i]] = i
-        
-        # we have to return the index, so sorting not allowed.
-        
-#         nums = sorted(nums)
-        
-#         l = 0
-#         r = len(nums) - 1
-#         while l < r:
-#             if nums[l] + nums[r] == target:
-#                 return([l,r])
-#             if nums[l] + nums[r] < target:
-#                 l += 1
-#             else:
-#                 r -= 1
-        
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        
+            n = nums[i]
+            if n in cache: return [i, cache[n]]
+            cache[target - n] = i
+
