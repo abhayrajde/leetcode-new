b2f07c5dc2722bc064f5b010461e3d1f30cb2088
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-19 18:22:21 +0000

    [LeetCode Sync] Runtime - 8 ms (60.78%), Memory - 16 MB (93.46%)

--- my-folder/0130-surrounded-regions/solution.py
+++ my-folder/0130-surrounded-regions/solution.py
@@ -1,48 +1,40 @@
 class Solution(object):
     def solve(self, board):
-        # STEP 1: CAPTURE UNSURROUNDED REGION- AND BY APPLYING DFS CONVERT ALL ELEMENTS INTO 'T'
-        rows, cols = len(board), len(board[0])
-        # visited = 
+        rows = len(board)
+        cols = len(board[0])
         def dfs(r,c):
-            if(r<0 or c<0 or
-              r == rows or c == cols or
-              board[r][c] == "T" or board[r][c] == "X"):
-                return
-            board[r][c] = "T"
-            dfs(r+1,c)
-            dfs(r-1,c)
-            dfs(r,c+1)
-            dfs(r,c-1)
-        
-        # VERTICAL TRAVERSAL
+            if (r >= 0 and r < rows and c >= 0 and c < cols 
+                and board[r][c] != "X" and board[r][c] != "T"):
+                board[r][c] = "T"
+                dfs(r+1,c)
+                dfs(r,c+1)
+                dfs(r-1,c)
+                dfs(r,c-1)
+
         for r in range(rows):
-            if(board[r][0] == "O"):
+            if board[r][0] == "O":
                 dfs(r,0)
-            if(board[r][cols-1] == "O"):
+            if board[r][cols-1] == "O":
                 dfs(r,cols-1)
-                
-        # HORIZONTAL TRAVERSAL
+
         for c in range(cols):
-            if(board[0][c] == "O"):
+            if board[0][c] == "O":
                 dfs(0,c)
-            if(board[rows-1][c] == "O"):
+            if board[rows-1][c] == "O":
                 dfs(rows-1,c)
-                
         
-        #2. CAPTURE SURROUNDED REGIONS - CONVERT THEM TO 'X'
         for r in range(rows):
             for c in range(cols):
-                if(board[r][c] == "O"):
+                if board[r][c] == "O":
                     board[r][c] = "X"
         
-        
-        #3. CONVERT THE UNSURROUNDED REGIONS FROM 'T' TO 'O'
         for r in range(rows):
             for c in range(cols):
-                if(board[r][c] == "T"):
+                if board[r][c] == "T":
                     board[r][c] = "O"
-        
-        
+
+
+
         """
         :type board: List[List[str]]
         :rtype: None Do not return anything, modify board in-place instead.
