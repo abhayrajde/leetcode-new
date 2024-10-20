19545a7965bb5e9f12c2b4556465c0574600b79f
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-20 22:11:30 +0000

    [LeetCode Sync] Runtime - 3 ms (65.95%), Memory - 16.6 MB (100.00%)

--- my-folder/0002-add-two-numbers/solution.py
+++ my-folder/0002-add-two-numbers/solution.py
@@ -1,34 +1,27 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        # l2 = self.reverse(l2)
-        # l1 = self.reverse(l1)
-        carry = 0
-        totsum = curr = ListNode(0)
-        while(l1 or l2 or carry):
-            v1 = l1.val if(l1) else 0
-            v2 = l2.val if l2 else 0
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        output = dummy
+
+        total = carry = 0
+        while l1 or l2 or carry:
+            total = carry
+
+            if l1:
+                total += l1.val
+                l1 = l1.next
+            if l2:
+                total += l2.val
+                l2 = l2.next
             
-            #new digit
-            temp = v1 + v2 + carry
-            carry = temp//10
-            temp = temp%10
+            num = total % 10
+            carry = total // 10
+            dummy.next = ListNode(num)
+            dummy = dummy.next
             
-            curr.next = ListNode(temp)
-            
-            curr = curr.next
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-            
-        return(totsum.next)
-            
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        
+        return output.next
