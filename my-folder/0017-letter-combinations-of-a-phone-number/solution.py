c387bd72121743bd8630fbf3fc3c34e99f87e2ac
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-22 23:54:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.7 MB (100.00%)

--- my-folder/0017-letter-combinations-of-a-phone-number/solution.py
+++ my-folder/0017-letter-combinations-of-a-phone-number/solution.py
@@ -1,32 +1,37 @@
-class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
-        if len(digits) == 0:
+class Solution(object):
+    def letterCombinations(self, digits):
+        if not digits:
             return []
-        mapping = {
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "pqrs",
-            "8": "tuv",
-            "9": "wxyz"
-        }
-
-        def backtrack(path, index):
-            if len(path) == len(digits):
-                combinations.append("".join(path))
+        hm = {'2':"abc", '3':"def", '4':"ghi", '5':"jkl", '6':"mno", '7':"pqrs", '8':"tuv", '9':"wxyz"}
+        res = []
+        # self.curr = ""
+        def backtrack(nums, curr):
+            if not nums:
+                res.append(curr)
                 return
 
-            possible = mapping[digits[index]]
-            for l in possible:
-                path.append(l)
-                backtrack(path, index + 1)
-                path.pop()
-            
-        combinations = []
-        backtrack([], 0)
-        return combinations
+            for i in hm[nums[0]]:
+                curr += i
 
-            
+                backtrack(nums[1:], curr)
 
+                curr = curr[:-1]
+
+        # def backtrack(nums):
+        #     if not nums:
+        #         res.append(self.curr[:])
+        #         return
+            
+        #     for j in hm[nums[0]]:
+        #         self.curr+=j
+        
+        #         backtrack(nums[1:])
+                
+        #         self.curr = self.curr[:-1]
+        backtrack(digits, "")
+        return(res)
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        
