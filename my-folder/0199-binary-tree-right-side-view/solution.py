8bd54672e8b55637e2faea1e77d3e75310705b03
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-22 04:11:27 +0000

    [LeetCode Sync] Runtime - 2 ms (17.05%), Memory - 11.6 MB (100.00%)

--- my-folder/0199-binary-tree-right-side-view/solution.py
+++ my-folder/0199-binary-tree-right-side-view/solution.py
@@ -1,31 +1,33 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+class Solution(object):
+    def rightSideView(self, root):
+        # Base Case:
         if not root:
             return []
-        cache = {}
-        q = [(root, 0, 0)]
-
-        while q:
-            node, row, col = q.pop(0)
-            if row in cache:
-                cache[row].append((node, col))
-            else:
-                cache[row] = [(node, col)]
-            if node.left:
-                q.append((node.left, row + 1, col - 1))
-            if node.right:
-                q.append((node.right, row + 1, col + 1))
+        q = deque([root])
+        result = []
         
-        output = []
-        for row in cache:
-            nodes = cache[row]
-            output.append(nodes[-1][0].val)
+        while q:
+            temp = []
+            for _ in range(len(q)):
+                curr = q.popleft()
+                temp.append(curr.val)
 
-        return output
+                if curr.left:
+                    q.append(curr.left)
 
+                if curr.right:
+                    q.append(curr.right)
+            result.append(temp[-1])
+        return result
+
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        
