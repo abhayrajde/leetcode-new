56b643acd7397c11d6b6ffcfe05237fc50730f4a
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-22 01:14:22 +0000

    [LeetCode Sync] Runtime - 11 ms (16.69%), Memory - 15.2 MB (26.50%)

--- my-folder/0210-course-schedule-ii/solution.py
+++ my-folder/0210-course-schedule-ii/solution.py
@@ -1,33 +1,31 @@
 class Solution(object):
     def findOrder(self, numCourses, prerequisites):
-        prereq = {c:[] for c in range(numCourses)}
-        
+        preMap = {c:[] for c in range(numCourses)}
         for crs,pre in prerequisites:
-            prereq[crs].append(pre)
-            
-        output = []
-        visited, cycle = set(), set()
+            preMap[crs].append(pre)
         
+        visited, cycle = set(), set()
+        result = []
+
         def dfs(crs):
+            #Base Cases
             if crs in cycle:
                 return False
             if crs in visited:
                 return True
-            
             cycle.add(crs)
-            for pre in prereq[crs]:
-                if(dfs(pre) == False):
+            for pre in preMap[crs]:
+                if dfs(pre) == False:
                     return False
             cycle.remove(crs)
             visited.add(crs)
-            output.append(crs)
+            result.append(crs)
             return True
         
-        for crs in prereq:
-            if(dfs(crs) == False):
+        for crs in range(numCourses):
+            if dfs(crs) == False:
                 return []
-        return output
-            
+        return result
         """
         :type numCourses: int
         :type prerequisites: List[List[int]]
