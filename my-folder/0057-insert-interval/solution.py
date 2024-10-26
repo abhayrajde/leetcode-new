199f200e63eccd2e4a84299fec56b7c79f60c340
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-26 18:54:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19 MB (100.00%)

--- my-folder/0057-insert-interval/solution.py
+++ my-folder/0057-insert-interval/solution.py
@@ -1,30 +1,20 @@
-class Solution(object):
-    def insert(self, intervals, newInterval):
-        # Edge Case - if list is empty
-        if(intervals == []):
-            intervals.append(newInterval)
-            return(intervals)
-            
-        for i in range(len(intervals)):
-                
-            if(intervals[i][0]>=newInterval[0]):
-                intervals.insert(i,newInterval)
-            
-            #Edge Case - if the newIntervals start is bigger than all the terms in the list
-            elif(i == len(intervals)-1):
-                intervals.append(newInterval)
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        n = len(intervals)
         i = 0
-        while(i<len(intervals)-1):
-            if(intervals[i][1]>=intervals[i+1][0]):
-                intervals[i] = [intervals[i][0],max(intervals[i+1][1],intervals[i][1])]
-                intervals.remove(intervals[i+1])
-                # i+=1
-            else:
-                i+=1
-        return(intervals)
-        """
-        :type intervals: List[List[int]]
-        :type newInterval: List[int]
-        :rtype: List[List[int]]
-        """
+        output = []
+
+        while i < n and intervals[i][1] < newInterval[0]:
+            output.append(intervals[i])
+            i += 1
         
+        while i < n and newInterval[1] >= intervals[i][0]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        output.append(newInterval)
+
+        while i < n:
+            output.append(intervals[i])
+            i += 1
+        return output
