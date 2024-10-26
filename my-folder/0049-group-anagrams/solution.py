64bbccb658770505912c6f5e2cbbf81c0c9217b6
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-26 00:11:11 +0000

    [LeetCode Sync] Runtime - 23 ms (11.27%), Memory - 21.6 MB (97.71%)

--- my-folder/0049-group-anagrams/solution.py
+++ my-folder/0049-group-anagrams/solution.py
@@ -1,16 +1,15 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        group = {}
 
-        hm = {}
-        for i in range(len(strs)):
-            temp = "".join(sorted(strs[i]))
-            if(temp not in hm):
-                hm[temp] = []
-            hm[temp].append(strs[i])
-        result = hm.values()
-        return result
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        
+        for s in strs:
+            indexes = [0] * 26
+            for c in s:
+                n = ord(c) - ord('a')
+                indexes[n] += 1
+            tup = tuple(indexes)
+            if tup in group:
+                group[tup].append(s)
+            else:
+                group[tup] = [s]
+        return list(group.values())
