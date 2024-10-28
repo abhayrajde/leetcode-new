a0b0e3c67bd6228fd317f4beaef6c2a99e590b8f
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-28 18:53:24 +0000

    [LeetCode Sync] Runtime - 15 ms (5.05%), Memory - 17.4 MB (100.00%)

--- my-folder/0152-maximum-product-subarray/solution.py
+++ my-folder/0152-maximum-product-subarray/solution.py
@@ -1,68 +1,16 @@
-class Solution(object):
-    def maxProduct(self, nums):
-        cmax,cmin = 1,1
-        max1 = max(nums)
-        for i in range(len(nums)):
-            
-            if(nums[i] == 0):
-                cmax,cmin = 1,1
-            
-            temp1 = cmax*nums[i]
-            temp2 = cmin*nums[i]
-            
-            cmax = max(temp1,temp2,nums[i])
-            cmin = min(temp1,temp2,nums[i])
-            
-            if(cmax > max1):
-                max1 = cmax
-        return(max1)
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-        """
-        currmax = 1
-        currmin = 1
-        max1 = max(nums)
-        for i in range(len(nums)):
-            if (nums[i] == 0):
-                currmax, currmin = 1,1
-            
-            temp1 = currmax * nums[i]
-            temp2 = currmin * nums[i]
-            currmax = max(temp1, temp2, nums[i])
-            currmin = min(temp1, temp2, nums[i])
-            max1 = max(currmax, max1)
-        return(max1)
-        """
-        # max1 = nums[0]
-        # prod = 1
-        # for i in range(len(nums)):
-        #     prod = prod*nums[i]
-        #     if(prod > max1):
-        #         max1 = prod
-        #     else:
-        #         prod = 1
-        # return(max1)
-                
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        if not nums: return 0
+
+        high = nums[0]
+        low = nums[0]
+        highest = high
         
+        for num in nums[1:]:
+            temp = max(num, max(high * num, low * num))
+            low = min(num, min(high * num, low * num))
+
+            high = temp
+            highest = max(high, highest)
+        
+        return highest
