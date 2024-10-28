c15beec8261f3dc3dc325f5be289a6003f4f8d28
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-28 18:07:05 +0000

    [LeetCode Sync] Runtime - 84 ms (5.13%), Memory - 30.9 MB (99.82%)

--- my-folder/0053-maximum-subarray/solution.py
+++ my-folder/0053-maximum-subarray/solution.py
@@ -1,21 +1,10 @@
-class Solution(object):
-    def maxSubArray(self, nums):
-        max1 = nums[0]
-        left, right = 0,0
-        currsum = 0
-        while(right<len(nums)):
-            
-            currsum+=nums[right]
-            max1 = max(max1, currsum)
-            
-            if(currsum < 0):
-                left = right+1
-                currsum = 0
-            
-            right = right+1
-        return(max1)
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        curr = highest = nums[0]
+
+        for n in nums[1:]:
+            curr = max(n, curr + n)
+            highest = max(highest, curr)
         
+        return highest
+
