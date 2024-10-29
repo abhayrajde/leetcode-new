3a69e4e3c967ac7a1e61cff2976d6d6c6ee3598e
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-29 22:47:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.5 MB (100.00%)

--- my-folder/0408-valid-word-abbreviation/solution.py
+++ my-folder/0408-valid-word-abbreviation/solution.py
@@ -1,29 +1,47 @@
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        # two pointer approach, one pointer on word and another on abbr
+        i = 0
+        j = 0
+        m = len(word)
+        n = len(abbr)
 
-class Solution(object):
-    def validWordAbbreviation(self, word, abbr):
-        wordPtr = abbrPtr = 0
-
-        while (wordPtr < len(word) and abbrPtr < len(abbr)):
-            if abbr[abbrPtr].isdigit():
-                steps = 0
-
-                if abbr[abbrPtr] == "0":
-                    return False
-
-                while (abbrPtr < len(abbr) and abbr[abbrPtr].isdigit()):
-                    steps = steps * 10 + int(abbr[abbrPtr])
-                    abbrPtr += 1
-                wordPtr += steps
+        while i < m and j < n:
+            if word[i] == abbr[j]: # if chars match, go forward
+                i += 1
+                j += 1
+            elif abbr[j] == '0': return False # if we encounter 0 return False
+            elif abbr[j].isnumeric(): # create int by looping all digits
+                k = j
+                while k < n and abbr[k].isnumeric():
+                    k += 1
+                i += int(abbr[j:k])
+                j = k
             else:
-                if word[wordPtr] != abbr[abbrPtr]:
-                    return False
-
-                wordPtr += 1
-                abbrPtr += 1
-        return wordPtr == len(word) and abbrPtr == len(abbr)
-        """
-        :type word: str
-        :type abbr: str
-        :rtype: bool
-        """
+                return False
         
+        return i == m and j == n
+
+
+        # length = ""
+        # for c in abbr:
+        #     if c.isnumeric():
+        #         if length == '' and c == '0': return False
+        #         length += c
+        #         continue
+
+        #     if length:
+        #         skipCount = int(length)
+        #         if len(word) < skipCount:
+        #             return False
+        #         word = word[skipCount:]
+        #         length = ""
+        #     if word != '' and c == word[0]:
+        #         word = word[1:]
+        #     else:
+        #         return False
+        # if length and length[0] == '0': return False
+        # if length == '':
+        #     length = '0'
+        # return length == '' or int(length) == len(word)
+
