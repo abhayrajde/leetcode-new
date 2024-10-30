96d09b2582d60df369a1a8e8effe6a940a9adaf8
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-30 00:43:15 +0000

    [LeetCode Sync] Runtime - 75 ms (22.47%), Memory - 20.8 MB (85.17%)

--- my-folder/0227-basic-calculator-ii/solution.py
+++ my-folder/0227-basic-calculator-ii/solution.py
@@ -1,32 +1,31 @@
-class Solution(object):
-    def calculate(self, s):
-        if not s:
-            return "0"
-        stack, num, sign = [], 0, "+"
-        for i in xrange(len(s)):
+class Solution:
+    def calculate(self, s: str) -> int:
+        # taking a stack and putting in the number if we get operation of + or -, 
+        # but if we got * or / we directly caluclate them with this operator, 
+        # at the end caluclate the sum of stack is answer
+
+        stack = []
+        prev_operator = "+"
+        curr_num = 0
+
+        for i in range(len(s)):
+
             if s[i].isdigit():
-                num = num*10+int(s[i])
-            if (not s[i].isdigit() and not s[i].isspace()) or i == len(s)-1:
-                if sign == "-":
-                    stack.append(-num)
-                elif sign == "+":
-                    stack.append(num)
-                elif sign == "*":
-                    stack.append(stack.pop()*num)
-                else:
-                    # temp = stack.pop()
-                    # stack.append(int(temp/num))
-                    tmp = stack.pop()
-                    if tmp//num < 0 and tmp%num != 0:
-                        stack.append(tmp//num+1)
-                    else:
-                        stack.append(tmp//num)
-                sign = s[i]
-                num = 0
-        return sum(stack)
-                
-        """
-        :type s: str
-        :rtype: int
-        """
+                curr_num = curr_num * 10 + int(s[i]) # what if we have double or triple digits
+            
+            if s[i] in "+-*/" or i == len(s) - 1:
+
+                if prev_operator == "+":
+                    stack.append(curr_num)
+                if prev_operator == "-":
+                    stack.append(-curr_num)
+                if prev_operator == "*":
+                    stack[-1] = stack[-1] * curr_num
+                if prev_operator == "/":
+                    stack[-1] = int(stack[-1] / curr_num) # don't use stack[-1]//curr_num it will give -5/2 to -3 but we want -2 so we use int, as it will remove anything after "." by giving -2
+
+                prev_operator = s[i]
+                curr_num = 0
         
+        return sum(stack)
+
