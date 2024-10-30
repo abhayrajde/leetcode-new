01da8cd99047020d7de93794bb2b77281a008205
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-30 02:09:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.7 MB (100.00%)

--- my-folder/0031-next-permutation/solution.py
+++ my-folder/0031-next-permutation/solution.py
@@ -1,38 +1,22 @@
-class Solution(object):
-    def nextPermutation(self, nums):
-        # if the length is 2 or less then 2 then we just need to reverse the number
-        if(len(nums)<=2):
-            return nums.reverse()
-        # STEP 1    from the last term traversing right to left and finding the downfall, ie 
-        # for eg: nums = [4,5,3,2,1] so 2>1, 3>2, 5>3, buttt 4!>5 ..now this is downfall
-        i = len(nums)-1
-        while(i>0 and nums[i] <= nums[i-1]):
-            i-=1
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        # Step 1: Find the first decreasing element from the end
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
         
-        # if we didnt found the downfall then reverse the whole string
-        if(i == 0):
-            return (nums.reverse())
-            
-        # STEP 2 : once we find the element with downfall, we need to find the no. just large than that element and swap the no.with that just large no.
-        for j in range(len(nums)-1,i-1,-1):
-            if(nums[j] > nums[i-1]):
-                nums[j],nums[i-1] = nums[i-1],nums[j]
-                break
-        # STEP 3 : now just sort the nos. just after the number which is swapped
-        nums[i:] = reversed(nums[i:])
-        return nums
-            
+        # Step 2: If we found a valid i, find the element to swap
+        if i >= 0:
+            j = len(nums) - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+            # Swap elements at i and j
+            nums[i], nums[j] = nums[j], nums[i]
         
-        """
-        for i in range(len(nums)-1,0,-1):
-            if(nums[i]>nums[i-1]):
-                nums[i],nums[i-1] = nums[i-1],nums[i]
-                break
-        else:
-            nums.sort()
-        """
-        """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
+        # Step 3: Reverse the elements from i + 1 to the end
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
         
