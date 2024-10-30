a380ffb5df17729f16c6ce1d1c66504fb75252af
Abhay Rajde <abhaythakkar1234@gmail.com>
2024-10-30 02:00:33 +0000

    [LeetCode Sync] Runtime - 42 ms (84.72%), Memory - 17.2 MB (100.00%)

---
+++ my-folder/0758-convert-binary-search-tree-to-sorted-doubly-linked-list/solution.py
@@ -0,0 +1,34 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+"""
+
+class Solution:
+    def treeToDoublyList(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        
+        def dfs(node):
+            nonlocal first, last
+            if node:
+                dfs(node.left)
+
+                if last:
+                    last.right = node
+                    node.left = last
+                else:
+                    first = node
+                last = node
+                dfs(node.right)
+            
+        if not root:
+            return None
+        
+        first, last = None, None    
+        dfs(root)
+
+        last.right = first
+        first.left = last
+        return first
