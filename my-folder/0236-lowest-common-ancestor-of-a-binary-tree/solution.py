e7b96f5d78f0d3f2bccb7be86610baf68a7908a5
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-01-03 03:37:08 +0000

    [LeetCode Sync] Runtime - 60 ms (12.38%), Memory - 27.2 MB (8.33%)

--- my-folder/0236-lowest-common-ancestor-of-a-binary-tree/solution.py
+++ my-folder/0236-lowest-common-ancestor-of-a-binary-tree/solution.py
@@ -1,21 +1,34 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def recurse_tree(current_node: TreeNode) -> bool:
-            if not current_node:
-                return False
-            left = recurse_tree(current_node.left)
-            right = recurse_tree(current_node.right)
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        def dfs(node):
+            # Base Case:
+            if not node:
+                return
+                
+            if node.val == p.val or node.val == q.val:
+                return node
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
 
-            mid = current_node == p or current_node == q
-            if mid + left + right >= 2:
-                self.ans = current_node
-            return mid or left or right
-        recurse_tree(root)
-        return self.ans
+            if left != None and right != None:
+                return node
+            
+            if left:
+                return left
+            return right
+        return dfs(root)
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        
