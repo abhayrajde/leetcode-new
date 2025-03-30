72bab4a237cc3a219134328833f6f26ba0424a9a
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-03-30 05:25:54 +0000

    [LeetCode Sync] Runtime - 9 ms (69.29%), Memory - 16.3 MB (34.45%)

--- my-folder/0056-merge-intervals/solution.py
+++ my-folder/0056-merge-intervals/solution.py
@@ -1,19 +1,22 @@
-class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # sort by start of each interval
-        # from index 1 to end, 
-        # if current's start <= prev's end, prev[1] = max(curr's last, prev's last)
-        # append prev at the very end
-        output = []
-        intervals.sort(key=lambda x: x[0])
+class Solution(object):
+    def merge(self, intervals):
+        result = []
+        intervals = sorted(intervals, key = lambda x:x[0])
 
         prev = intervals[0]
-        for i in range(1, len(intervals)):
-            interval = intervals[i]
-            if interval[0] <= prev[1]:
-                prev[1] = max(prev[1], interval[1])
+        
+        for i in range(1,len(intervals)):
+            curr = intervals[i]
+            if prev[1] >= curr[0]:
+                prev[1] = max(prev[1], curr[1])
             else:
-                output.append(prev)
-                prev = interval
-        output.append(prev)
-        return output
+                result.append(prev)
+                prev = curr
+        result.append(prev)
+        return result
+
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        
