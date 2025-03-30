5d640e3c2e84d84db9c79ef8a8f45315f432dba6
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-03-30 23:47:06 +0000

    [LeetCode Sync] Runtime - 426 ms (35.69%), Memory - 12.6 MB (9.52%)

--- my-folder/0005-longest-palindromic-substring/solution.py
+++ my-folder/0005-longest-palindromic-substring/solution.py
@@ -1,41 +1,35 @@
 class Solution(object):
     def longestPalindrome(self, s):
-        # if(len(s) == 1):
-        #     return(s)
-        # else:
-        pal = ""
-        pal_len = 0
-        
-        # In this loop i will be the center of palindrome
-        for mid in range(len(s)):
-            # if (mid == 0):
-            #     pal = s[0]
-            
-            # else:    
-            # for odd palindrome
-            i = mid
-            j = mid
-            while(i>=0 and j<len(s) and s[i] == s[j]):
-                if(j-i+1>pal_len):
-                    pal = s[i:j+1]
-                    pal_len = len(pal)
-                i-=1
-                j+=1
+        res = ""
+        resLen = [0]
+        # def updateLongestPalindrome(l,r):
+        #     while l >= 0 and r < len(s) and s[l] == s[r]:
+        #         if (r - l + 1) > resLen[0]:
+        #             res = s[l:r+1]
+        #             resLen[0] = len(res)
+        #         l -= 1
+        #         r += 1
+        for i in range(len(s)):
+            # updateLongestPalindrome(i,i)
+            # updateLongestPalindrome(i,i+1)
+            l, r = i,i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen[0]:
+                    res = s[l:r+1]
+                    resLen[0] = len(res)
+                l -= 1
+                r += 1
 
-
-            # for even length palindrome
-            i = mid
-            j = mid+1
-            while(i>=0 and j<len(s) and s[i] == s[j]):
-                if(j-i+1>pal_len):
-                    pal = s[i:j+1]
-                    pal_len = len(pal)
-                i-=1
-                j+=1
-        return(pal)
-
+            l, r = i,i+1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen[0]:
+                    res = s[l:r+1]
+                    resLen[0] = len(res)
+                l -= 1
+                r += 1
+        return res
             
-            
+
         """
         :type s: str
         :rtype: str
