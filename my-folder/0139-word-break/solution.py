a3b23ea24a7ff097b35cd0ec45e509d00652e180
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-03-31 01:47:45 +0000

    [LeetCode Sync] Runtime - 3 ms (89.57%), Memory - 12.4 MB (74.55%)

--- my-folder/0139-word-break/solution.py
+++ my-folder/0139-word-break/solution.py
@@ -1,15 +1,16 @@
 class Solution(object):
     def wordBreak(self, s, wordDict):
-        dp = [False]*(len(s)+1)
-        dp[len(s)] = True
-        
-        for i in range(len(s),-1,-1):
-            for word in wordDict:
-                if (i+len(word)) <=len(s) and s[i : i+len(word)] == word:
-                    dp[i] = dp[i+len(word)]
-                if(dp[i]):
+        dp = [False for i in range(len(s)+1)]
+        dp[-1] = True
+
+        for i in range(len(s)-1, -1, -1):
+            for w in wordDict:
+                if i + len(w) <= len(s) and s[i : i + len(w)] == w:
+                    dp[i] = dp[i + len(w)]
+                if dp[i]:
                     break
-        return(dp[0])
+        return dp[0]
+
         """
         :type s: str
         :type wordDict: List[str]
