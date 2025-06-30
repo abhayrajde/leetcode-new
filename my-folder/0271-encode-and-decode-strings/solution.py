4f76702350ccc84c73e08ff3b315607d4599a155
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-06-30 19:24:41 +0000

    [LeetCode Sync] Runtime - 54 ms (58.41%), Memory - 12.8 MB (17.29%)

--- my-folder/0271-encode-and-decode-strings/solution.py
+++ my-folder/0271-encode-and-decode-strings/solution.py
@@ -15,16 +15,17 @@ class Codec:
     def decode(self, s):
         res = []
         i = 0
-        while(i < len(s)):
+        while i < len(s):
             j = i
-            while(s[j] != "#"):
+            while s[j] != '#':
                 j += 1
             length = int(s[i:j])
-            word = s[j+1:j+1+length]
+            word = s[j+1:j+length+1]
             res.append(word)
             i = j+1+length
         return res
 
+                
         """Decodes a single string to a list of strings.
         
         :type s: str
