c934ebe921cfc4d0875b590aefc0ff9fd2a9f3c6
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-05 21:59:37 +0000

    [LeetCode Sync] Runtime - 1029 ms (20.18%), Memory - 18.2 MB (94.06%)

--- my-folder/0015-3sum/solution.py
+++ my-folder/0015-3sum/solution.py
@@ -3,24 +3,22 @@ class Solution(object):
         res = []
         nums.sort()
 
-        for i in range(len(nums)-2):
-            if i > 0 and nums[i] == nums[i-1]:
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i - 1]:
                 continue
-
             l = i + 1
             r = len(nums) - 1
             while l < r:
                 if nums[l] + nums[r] + nums[i] == 0:
                     res.append([nums[i], nums[l], nums[r]])
                     l += 1
-                    while nums[l] == nums[l-1] and l < r: l += 1
-
-                elif nums[l] + nums[r] + nums[i] < 0:
-                    l += 1
-                else:
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1
+                elif nums[l] + nums[r] + nums[i] > 0:
                     r -= 1
+                else:
+                    l += 1
         return res
-
         """
         :type nums: List[int]
         :rtype: List[List[int]]
