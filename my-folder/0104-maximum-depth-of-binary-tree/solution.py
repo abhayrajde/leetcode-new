eae5ab29e28ca87d4e2176d6fce11abd5dc2bdef
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-13 04:40:17 +0000

    [LeetCode Sync] Runtime - 4 ms (6.56%), Memory - 19.1 MB (100.00%)

--- my-folder/0104-maximum-depth-of-binary-tree/solution.py
+++ my-folder/0104-maximum-depth-of-binary-tree/solution.py
@@ -1,23 +1,22 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def maxDepth(self, root):
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
         res = [0]
-        def dfs(node,currLength):
+        def dfs(node, currDepth):
             if not node:
                 return
-            currLength += 1
-            res[0] = max(res[0],currLength)
-            dfs(node.left, currLength)
-            dfs(node.right, currLength)
-        dfs(root,0)
+
+            res[0] = max(res[0], currDepth + 1)
+            dfs(node.left, currDepth + 1)
+            dfs(node.right, currDepth + 1)
+
+        dfs(root, 0)
         return res[0]
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        
+
+
+            
