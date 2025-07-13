16d1e6ede0c80fd9fae72eeca2748788e58eeb59
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-13 06:10:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (100.00%)

--- my-folder/0100-same-tree/solution.py
+++ my-folder/0100-same-tree/solution.py
@@ -1,25 +1,19 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def isSameTree(self, p, q):
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
         if (not p and not q):
             return True
         
-        if (not p or not q):
+        if (not p or not q or p.val != q.val):
             return False
         
-        if (p.val != q.val):
-            return False
-        
-        if (self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)):
-            return True
-        """
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: bool
-        """
+        return (self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right))
+
+
         
