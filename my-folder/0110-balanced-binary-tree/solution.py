a11bc24e015ba88c1c0a0dc4fd0e6d0194e91244
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-13 05:31:41 +0000

    [LeetCode Sync] Runtime - 3 ms (52.91%), Memory - 19 MB (100.00%)

--- my-folder/0110-balanced-binary-tree/solution.py
+++ my-folder/0110-balanced-binary-tree/solution.py
@@ -1,26 +1,25 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def isBalanced(self, root):
-        res = [True]
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        self.res = True
+
         def dfs(node):
             if not node:
                 return 0
+            
             left = dfs(node.left)
             right = dfs(node.right)
-            
-            if(abs(left-right)>1):
-                res[0] = False
-            height = 1 + max(left,right)
+
+            if abs(left - right) > 1:
+                self.res = False
+            height = 1 + max(left, right)
             return height
         dfs(root)
-        return res[0]
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
+        return self.res
+
         
