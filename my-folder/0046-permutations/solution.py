f33156785920b4662e31c648f5051351f1d974a8
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-14 22:03:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (100.00%)

--- my-folder/0046-permutations/solution.py
+++ my-folder/0046-permutations/solution.py
@@ -1,40 +1,20 @@
-class Solution(object):
-    def permute(self, nums): 
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
-        
-        def backtrack(ind,nums):
-            if(ind == len(nums)):
-                res.append(nums[:])
+        isTaken = [False] * len(nums)
+
+        def dfs(curr, isTaken):
+            if len(curr) == len(nums):
+                res.append(curr[:])
                 return
             
-            for i in range(ind,len(nums)):
-                nums[ind],nums[i] = nums[i],nums[ind]
-                backtrack(ind+1,nums)
-                nums[ind],nums[i] = nums[i],nums[ind]
-        backtrack(0,nums)
-        return res
-                
-        
-        #Space Complexity  - O(N!) * N
-#         res = []
-#         freq = [0]*len(nums)
-#         curr = []
-#         def backtrack(nums,curr,res,freq):
-#             if (len(curr) == len(nums)):
-#                 res.append(curr[:])
-#                 return
+            for i in range(len(nums)):
+                if not isTaken[i]:
+                    isTaken[i] = True
+                    curr.append(nums[i])
+                    dfs(curr, isTaken)
+                    curr.pop()
+                    isTaken[i] = False
             
-#             for i in range(len(nums)):
-#                 if(freq[i] == 0):
-#                     freq[i] = 1
-#                     curr.append(nums[i])
-#                     backtrack(nums,curr,res,freq)
-#                     curr.pop()
-#                     freq[i] = 0
-#         backtrack(nums,curr,res,freq)
-#         return res
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        
+        dfs([],isTaken)
+        return res
