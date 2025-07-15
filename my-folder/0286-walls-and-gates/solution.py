09895e93069680a69c91f5a38cee1b5bbbbadbb9
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-15 06:00:21 +0000

    [LeetCode Sync] Runtime - 51 ms (33.21%), Memory - 25 MB (30.03%)

--- my-folder/0286-walls-and-gates/solution.py
+++ my-folder/0286-walls-and-gates/solution.py
@@ -1,31 +1,36 @@
-class Solution(object):
-    def wallsAndGates(self, rooms):
-        rows = len(rooms)
-        cols = len(rooms[0])
-        q = deque([])
+class Solution:
+    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+        ROWS = len(rooms)
+        COLS = len(rooms[0])
+        q = deque()
         visited = set()
 
-        for r in range(rows):
-            for c in range(cols):
+        def addRoom(r, c):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r, c) in visited or rooms[r][c] == -1:
+                return 
+            visited.add((r,c))
+            q.append((r,c))
+
+        for r in range(ROWS):
+            for c in range(COLS):
                 if rooms[r][c] == 0:
-                    q.append((r,c,0))
-        while(q):
-            r, c, dist = q.popleft()
-            if(r >= 0 and r < rows and c >= 0 and c < cols
-                and (r,c) not in visited and rooms[r][c] != -1):
-                visited.add((r,c))
-                q.append((r+1,c,dist+1))
-                q.append((r-1,c,dist+1))
-                q.append((r,c+1,dist+1))
-                q.append((r,c-1,dist+1))
-                rooms[r][c] = min(rooms[r][c], dist)
-        return rooms
-                    
-                
+                    q.append((r,c))
+                    visited.add((r,c))
+            
+        dist = 0
+        while q:
+            for _ in range(len(q)):
+                r, c = q.popleft()
+                rooms[r][c] = dist
+                addRoom(r + 1, c)
+                addRoom(r - 1, c)
+                addRoom(r, c + 1)
+                addRoom(r, c - 1)
+
+            dist += 1
 
 
         """
-        :type rooms: List[List[int]]
-        :rtype: None Do not return anything, modify rooms in-place instead.
+        Do not return anything, modify rooms in-place instead.
         """
         
