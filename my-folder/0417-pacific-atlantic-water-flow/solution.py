e86e3075282abf6ec7c0266100fa6acf6db6e83c
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-15 21:28:04 +0000

    [LeetCode Sync] Runtime - 35 ms (40.36%), Memory - 19.3 MB (100.00%)

--- my-folder/0417-pacific-atlantic-water-flow/solution.py
+++ my-folder/0417-pacific-atlantic-water-flow/solution.py
@@ -1,39 +1,34 @@
-class Solution(object):
-    def pacificAtlantic(self, heights):
-        rows = len(heights)
-        cols = len(heights[0])
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        ROWS = len(heights)
+        COLS = len(heights[0])
 
-        pac = set()
-        atl = set()
+        atl, pac = set(), set()
 
-        def dfs(r,c, visited, prevHeight):
-            if (r < 0 or r == rows or c < 0 or c == cols
-                or (r,c) in visited or heights[r][c] < prevHeight):
-                return
-            visited.add((r,c))
-            dfs(r + 1,c,visited,heights[r][c])
-            dfs(r - 1,c,visited,heights[r][c])
-            dfs(r,c + 1,visited,heights[r][c])
-            dfs(r,c - 1,visited,heights[r][c])
-
-        for c in range(cols):
-            dfs(0,c,pac,-1)
-            dfs(rows-1,c,atl,-1)
-        
-        for r in range(rows):
-            dfs(r,0,pac,-1)
-            dfs(r,cols-1,atl,-1)
-        
         res = []
-        for r in range(rows):
-            for c in range(cols):
+
+        def dfs(r, c, visited, prevHeight):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r,c) in visited or  heights[r][c] < prevHeight:
+                return
+            
+            visited.add((r,c))
+            dfs(r + 1, c, visited, heights[r][c])
+            dfs(r - 1, c, visited, heights[r][c])
+            dfs(r, c + 1, visited, heights[r][c])
+            dfs(r, c - 1, visited, heights[r][c])
+
+        for c in range(COLS):
+            dfs(0, c, pac, heights[0][c])
+            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
+        
+        for r in range(ROWS):
+            dfs(r, 0, pac, heights[r][0])
+            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
+        
+        for r in range(ROWS):
+            for c in range(COLS):
                 if (r,c) in pac and (r,c) in atl:
                     res.append([r,c])
         return res
 
-        
-        """
-        :type heights: List[List[int]]
-        :rtype: List[List[int]]
-        """
         
