e9d9d1d4e801e6d624595de473f9f640ddb7baf9
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-15 02:09:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.8 MB (100.00%)

--- my-folder/0017-letter-combinations-of-a-phone-number/solution.py
+++ my-folder/0017-letter-combinations-of-a-phone-number/solution.py
@@ -1,37 +1,20 @@
-class Solution(object):
-    def letterCombinations(self, digits):
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
-        hm = {'2':"abc", '3':"def", '4':"ghi", '5':"jkl", '6':"mno", '7':"pqrs", '8':"tuv", '9':"wxyz"}
         res = []
-        # self.curr = ""
-        def backtrack(nums, curr):
-            if not nums:
-                res.append(curr)
+        hm = {'2':"abc", '3':"def", '4':"ghi", '5':"jkl", '6':"mno", '7':"pqrs", '8':"tuv", '9':"wxyz"}
+
+        def dfs(ind, curr):
+            if ind >= len(digits):
+                res.append("".join(curr[:]))
                 return
-
-            for i in hm[nums[0]]:
-                curr += i
-
-                backtrack(nums[1:], curr)
-
-                curr = curr[:-1]
-
-        # def backtrack(nums):
-        #     if not nums:
-        #         res.append(self.curr[:])
-        #         return
             
-        #     for j in hm[nums[0]]:
-        #         self.curr+=j
-        
-        #         backtrack(nums[1:])
-                
-        #         self.curr = self.curr[:-1]
-        backtrack(digits, "")
-        return(res)
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
+            for j in hm[digits[ind]]:
+                curr.append(j)
+                dfs(ind + 1, curr)
+                curr.pop()
+            
+        dfs(0, [])
+        return res
         
