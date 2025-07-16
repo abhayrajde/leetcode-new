e3c1cb729f2837806058fb5cca079a3411bba198
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-16 21:54:46 +0000

    [LeetCode Sync] Runtime - 1 ms (70.17%), Memory - 18.1 MB (100.00%)

--- my-folder/0684-redundant-connection/solution.py
+++ my-folder/0684-redundant-connection/solution.py
@@ -1,36 +1,34 @@
-class Solution(object):
-    def findRedundantConnection(self, edges):
-        par = []
-        for i in range(len(edges)+1):
-            par.append(i)
-        
-        rank = [1] * (len(edges)+1)
-        
-        def find(n):
-            p = par[n]
-            while(p!=par[p]):
-                p = par[p]
-            return p
-        
-        def union(n1,n2):
-            p1,p2 = find(n1), find(n2)
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        par = [i for i in range(n+1)]
+        rank = [1] * (n+1)
+
+        def find(node): # finding the root parent
+            res = node
+            while res != par[res]:
+                par[node] = par[par[node]]
+                res = par[node]
+            return res
+
+
+        def union(n1, n2): # mapping to root parent of both nodes
+            p1, p2 = find(n1), find(n2)
             
-            if(p1 == p2):
-                return(False)
+            if p1 == p2:
+                return False
             
-            if(rank[p1] > rank[p2]):
+            if rank[p1] > rank[p2]:
                 par[p2] = p1
                 rank[p1] += rank[p2]
             else:
                 par[p1] = p2
                 rank[p2] += rank[p1]
-            return(True)
+            return True
         
-        for n1,n2 in edges:
+        for n1, n2 in edges:
             if not union(n1,n2):
-                return([n1,n2])
-        """
-        :type edges: List[List[int]]
-        :rtype: List[int]
-        """
+                return [n1,n2]
+
+
         
