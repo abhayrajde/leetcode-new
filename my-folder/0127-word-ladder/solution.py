8f6c82e8d0c729dd2756efb77586389ef0586384
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-16 22:34:38 +0000

    [LeetCode Sync] Runtime - 51 ms (88.52%), Memory - 20.8 MB (45.36%)

--- my-folder/0127-word-ladder/solution.py
+++ my-folder/0127-word-ladder/solution.py
@@ -1,43 +1,33 @@
-from collections import defaultdict
-from collections import deque
-class Solution(object):
-    def ladderLength(self, beginWord, endWord, wordList):
-        #Base Case
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        # Edge/Base case
         if endWord not in wordList:
             return 0
-        
-        nei = defaultdict(list)
+
+        nei = collections.defaultdict(list)
         wordList.append(beginWord)
+        
         for word in wordList:
-            for j in range(len(word)):
-                pattern = word[:j] + "*" + word[j+1:]
+            for i in range(len(word)):
+                pattern = word[:i] + "*" + word[i + 1:]
                 nei[pattern].append(word)
         
         visited = set([beginWord])
         q = deque([beginWord])
         res = 1
-        
+
         while q:
-            for i in range(len(q)):
+            for _ in range(len(q)):
                 word = q.popleft()
                 if word == endWord:
                     return res
-                for j in range(len(word)):
-                    pattern = word[:j] + "*" + word[j+1:] 
-                    for neiword in nei[pattern]:
-                        if neiword not in visited:
-                            visited.add(neiword)
-                            q.append(neiword)
-                             
-            res+=1
-        
-        return 0 
                 
-        
-        """
-        :type beginWord: str
-        :type endWord: str
-        :type wordList: List[str]
-        :rtype: int
-        """
+                for j in range(len(word)):
+                    pattern = word[:j] + '*' + word[j + 1:]
+                    for neiWord in nei[pattern]:
+                        if neiWord not in visited:
+                            visited.add(neiWord)
+                            q.append(neiWord)
+            res += 1
+        return 0
         
