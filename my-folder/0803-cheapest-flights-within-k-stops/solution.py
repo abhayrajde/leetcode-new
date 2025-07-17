486eb2ef22a828e405e86009e32035de7303481d
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-17 05:16:59 +0000

    [LeetCode Sync] Runtime - 94 ms (20.10%), Memory - 19 MB (100.00%)

--- my-folder/0803-cheapest-flights-within-k-stops/solution.py
+++ my-folder/0803-cheapest-flights-within-k-stops/solution.py
@@ -1,25 +1,15 @@
-# import Copy
-class Solution(object):
-    def findCheapestPrice(self, n, flights, src, dst, k):
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         prices = [float("inf")] * n
         prices[src] = 0
-        for i in range(k+1):
-            temp = copy.copy(prices)
-            for s,d,p in flights:
-                if(prices[s] == float("inf")):
+
+        for i in range(k + 1):
+            tmpPrices = prices.copy()
+
+            for s, d, p in flights:  # s=source, d=dest, p=price
+                if prices[s] == float("inf"):
                     continue
-                if(prices[s] + p < temp[d]):
-                    temp[d] = prices[s] + p
-            prices = temp
-        if(prices[dst] == float("inf")):
-            return(-1)
-        return(prices[dst])
-        """
-        :type n: int
-        :type flights: List[List[int]]
-        :type src: int
-        :type dst: int
-        :type k: int
-        :rtype: int
-        """
-        
+                if prices[s] + p < tmpPrices[d]:
+                    tmpPrices[d] = prices[s] + p
+            prices = tmpPrices
+        return -1 if prices[dst] == float("inf") else prices[dst]
