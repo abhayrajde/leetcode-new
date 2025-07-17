1971386a94bb382c31a6c994fa0837bd314380fe
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-17 21:53:06 +0000

    [LeetCode Sync] Runtime - 3 ms (65.95%), Memory - 17.7 MB (100.00%)

--- my-folder/0002-add-two-numbers/solution.py
+++ my-folder/0002-add-two-numbers/solution.py
@@ -1,33 +1,27 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode()
         res = dummy
 
         carry = 0
         while l1 or l2 or carry:
-            curr = carry
+            currVal = carry
             if l1:
-                curr += l1.val
+                currVal += l1.val
                 l1 = l1.next
             if l2:
-                curr += l2.val
+                currVal += l2.val
                 l2 = l2.next
-            
-            carry = curr // 10
-            value = curr % 10
+            value = currVal % 10
+            carry = currVal // 10
+
             dummy.next = ListNode(value)
-
             dummy = dummy.next
-
         return res.next
-        """
-        :type l1: Optional[ListNode]
-        :type l2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        
+            
+            
