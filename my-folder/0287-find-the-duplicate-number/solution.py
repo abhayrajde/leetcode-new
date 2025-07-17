e4e62eeae979cc22c28fd40d3c316b84dc26ca38
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-17 23:10:23 +0000

    [LeetCode Sync] Runtime - 24 ms (67.74%), Memory - 30 MB (99.98%)

--- my-folder/0287-find-the-duplicate-number/solution.py
+++ my-folder/0287-find-the-duplicate-number/solution.py
@@ -1,21 +1,25 @@
-class Solution(object):
-    def findDuplicate(self, nums):
-        slow = 0
-        fast = 0
-        while(True):
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        # How can we prove that cycle's start point is repeating char:
+        # 2 * Slow = Fast
+        # 2 * (P + C - X) = P + 2C - X | P = previos nodes, C = Cycles, X = remaining nodes(from meeting point to start of cycle)
+        # 2P + 2C - 2X = P + 2C - X
+        # P - X = 0
+        # P = X
+
+        slow, fast = 0, 0
+
+        while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
-            if(slow == fast):
+            if slow == fast:
                 break
         
         slow2 = 0
-        while(True):
+        while True:
             slow = nums[slow]
             slow2 = nums[slow2]
-            if(slow == slow2):
-                return(slow)
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
+            if slow == slow2:
+                break
+        return slow
+
