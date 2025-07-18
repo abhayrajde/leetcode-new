fe31d9013a30602221e39a996d6531d9d5e3e969
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-18 01:06:14 +0000

    [LeetCode Sync] Runtime - 10 ms (59.65%), Memory - 19.8 MB (100.00%)

--- my-folder/0023-merge-k-sorted-lists/solution.py
+++ my-folder/0023-merge-k-sorted-lists/solution.py
@@ -1,45 +1,42 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeKLists(self, lists):
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # similar concept like merge sort - merging all lists simultaneously in a pair
+        # TC - NlogK
+
+        #Base case
+        if not lists or len(lists) == 0:
+            return None
         
-        if( not lists or len(lists)==0):
-            return(None)
-        
-        while(len(lists)>1):
-            mergelist = []
-            for i in range(0,len(lists),2):
-                l1 = lists[i]
-                if((i+1 < len(lists))):
-                    l2 = lists[i+1]
+        def merge(l1, l2):
+            res = ListNode()
+            tail = res
+
+            while l1 and l2:
+                if l1.val < l2.val:
+                    tail.next = l1
+                    l1 = l1.next
                 else:
-                    l2 = None
-                mergelist.append(self.merge(l1,l2))
-            lists = mergelist
-        return(lists[0])
-    
-    def merge(self,l1,l2):
-        temp = ListNode()
-        op = temp
-        while(l1 and l2):
-            if(l1.val < l2.val):
-                op.next = l1
-                l1 = l1.next
+                    tail.next = l2
+                    l2 = l2.next
+                tail = tail.next
+            if l1:
+                tail.next = l1
             else:
-                op.next = l2
-                l2 = l2.next
-            op = op.next
-        if(l1):
-            op.next = l1
-        else:
-            op.next = l2
-        return(temp.next)
-        #TODO
-        """
-        :type lists: List[ListNode]
-        :rtype: ListNode
-        """
-        
+                tail.next = l2
+            return res.next
+            
+        while len(lists) > 1:
+            mergedLists = []
+
+            for i in range(0, len(lists), 2):
+                list1 = lists[i]
+                list2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mergedLists.append(merge(list1, list2))
+            lists = mergedLists
+        return lists[0]
+
