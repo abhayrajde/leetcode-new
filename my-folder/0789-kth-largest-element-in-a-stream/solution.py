767c22c784b01ff7b577e0461a4e5cc87a2378be
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-18 02:54:39 +0000

    [LeetCode Sync] Runtime - 18 ms (53.06%), Memory - 23.7 MB (100.00%)

--- my-folder/0789-kth-largest-element-in-a-stream/solution.py
+++ my-folder/0789-kth-largest-element-in-a-stream/solution.py
@@ -1,34 +1,17 @@
-class KthLargest(object):
+class KthLargest:
 
-    def __init__(self, k, nums):
-        self.minheap = nums
-        self.k = k 
-        heapq.heapify(self.minheap)
-        while(len(self.minheap) > self.k):
-            heapq.heappop(self.minheap)
-        """
-        :type k: int
-        :type nums: List[int]
-        """
-        
+    def __init__(self, k: int, nums: List[int]):
+        # minHeap with k largest integers
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
 
-    def add(self, val):
-        heapq.heappush(self.minheap, val)
-        if(len(self.minheap) > self.k):
-            heapq.heappop(self.minheap)
-        return(self.minheap[0])
-        """
-        :type val: int
-        :rtype: int
-        """
-        
-
-
-# Your KthLargest object will be instantiated and called as such:
-# obj = KthLargest(k, nums)
-# param_1 = obj.add(val)
-        
-
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
