e6473e94bb0d32a9c12dd90957a5ac0155f5fde0
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-18 03:28:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (100.00%)

--- my-folder/1127-last-stone-weight/solution.py
+++ my-folder/1127-last-stone-weight/solution.py
@@ -1,26 +1,14 @@
-class Solution(object):
-    def lastStoneWeight(self, stones):
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        maxHeap = []
+        for st in stones:
+            heapq.heappush(maxHeap, -st)
         
-        #Python doesnt have maxheap so using the concept of minheap
-        for i in range(len(stones)):
-            stones[i] = -1*stones[i]
+        while len(maxHeap) > 1:
+            y = - heapq.heappop(maxHeap)
+            x = - heapq.heappop(maxHeap)
+            if y > x:
+                heapq.heappush(maxHeap, -(y - x))
+        return -maxHeap[0] if maxHeap else 0
             
-        heapq.heapify(stones)
-        
-        while(len(stones) > 1):
-            x = heapq.heappop(stones)
-            y = heapq.heappop(stones)
-            if(y > x):
-                x = abs(x)
-                y = abs(y)
-                heapq.heappush(stones,y - x)
-        if(len(stones)==0):
-            return(0)    
-        return(-1*(stones[0]))
-            
-            
-        """
-        :type stones: List[int]
-        :rtype: int
-        """
-        
+
