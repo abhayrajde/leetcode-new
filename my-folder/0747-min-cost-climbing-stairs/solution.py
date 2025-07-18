ce0278fe24d9e13f8f21f7ce92ed5e674bd1b635
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-18 19:18:37 +0000

    [LeetCode Sync] Runtime - 8 ms (7.22%), Memory - 19 MB (99.52%)

--- my-folder/0747-min-cost-climbing-stairs/solution.py
+++ my-folder/0747-min-cost-climbing-stairs/solution.py
@@ -1,13 +1,30 @@
-class Solution(object):
-    def minCostClimbingStairs(self, cost):
-        cost.append(0)
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        #DP: Memoization
+        dp = [-1] * (len(cost) + 1) 
+        def dpmem(ind):
+            if ind < 0: 
+                return 0
+
+            if dp[ind] != -1:
+                return dp[ind]
+
+            currCost = 0 if ind == len(cost) else cost[ind]
+            dp[ind] = currCost + min(dpmem(ind - 1), dpmem(ind - 2))
+            return dp[ind]
+
+
+        # Recursion
+        def recursion(ind):
+            if ind < 0:
+                return 0
+
+            currCost = 0 if ind == len(cost) else cost[ind]
+            
+            option1 = currCost + recursion(ind - 1)
+            option2 = currCost + recursion(ind - 2)           
+            return min(option1, option2)
         
-        for i in range(len(cost)-3,-1,-1):
-            cost[i] += min(cost[i+1],cost[i+2])
+        return min(dpmem(len(cost)), dpmem(len(cost)-1)) 
         
-        return min(cost[0],cost[1])
-        """
-        :type cost: List[int]
-        :rtype: int
-        """
         
