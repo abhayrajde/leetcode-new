d809d05ac0cbdce53071d7934045ab1487442eb5
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-19 23:20:35 +0000

    [LeetCode Sync] Runtime - 11 ms (28.82%), Memory - 18.6 MB (100.00%)

---
+++ my-folder/0045-jump-game-ii/solution.py
@@ -0,0 +1,33 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        # GREEDY - TC: O(N)
+        res = 0 
+        l, r = 0, 0 
+
+        while r < len(nums) - 1:
+            farthest = 0
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+            l = r + 1
+            r = farthest
+            res += 1
+        return res
+
+        # DP: MEMOIZATION - TOP DOWN - TC: O(N)**2
+        # dp = [-1] * len(nums)
+        # def dpmem(ind): # no. of jumps
+        #     if ind >= len(nums) - 1:
+        #         return 0
+        #     if nums[ind] == 0:
+        #         return float("inf")
+        #     if dp[ind] != -1:
+        #         return dp[ind]
+
+        #     minJumps = float("inf")
+        #     for i in range(1, nums[ind] + 1):
+        #         jumps = 1 + dpmem(ind + i)
+        #         minJumps = min(minJumps, jumps)
+        #     dp[ind] = minJumps
+        #     return dp[ind]
+        # return dpmem(0)
+
