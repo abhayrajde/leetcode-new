292faf0b8c4541f7583bac75af057c75ffb5feea
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-19 00:00:06 +0000

    [LeetCode Sync] Runtime - 9800 ms (5.00%), Memory - 225.9 MB (5.06%)

--- my-folder/0300-longest-increasing-subsequence/solution.py
+++ my-folder/0300-longest-increasing-subsequence/solution.py
@@ -1,13 +1,48 @@
-class Solution(object):
-    def lengthOfLIS(self, nums):
-        dp = [1] * len(nums)
-        for i in range(len(nums)-1,-1,-1):
-            for j in range (i+1,len(nums)):
-                if(nums[i]<nums[j]):
-                    dp[i] = max(dp[i],1+dp[j])
-        return(max(dp))
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # DP: Memoization
+        dp = [[-1] * (len(nums)+1) for _ in range(len(nums)+1)]
+
+        def dpmem(ind, prevInd):
+            if ind >= len(nums):
+                return 0
+            if dp[ind][prevInd] != -1:
+                return dp[ind][prevInd]
+
+            notTake = dpmem(ind + 1, prevInd)
+            take = 0
+            if prevInd == -1 or nums[ind] > nums[prevInd]:
+                take = 1 + dpmem(ind + 1, ind)
+            dp[ind][prevInd] = max(take, notTake)
+            return dp[ind][prevInd]
+
+        # def dpmem(ind, prevInd): #Return -> count of elements
+        #     if ind < 0:
+        #         return 0
+            
+        #     if dp[ind][prevInd] != -1:
+        #         return dp[ind][prevInd]
+
+        #     notTake = 0 + dpmem(ind - 1, prevInd)
+        #     take = -float("inf")
+        #     if nums[ind] < nums[prevInd]:
+        #         take = 1 + dpmem(ind - 1, ind)
+
+        #     dp[ind][prevInd] = max(take, notTake)
+        #     return dp[ind][prevInd]
         
+        return dpmem(0, -1)
+
+        # Recursion
+        def recursion(ind, prevEl): #Return -> count of elements
+            if ind < 0:
+                return 0
+
+            notTake = 0 + recursion(ind - 1, prevEl)
+            take = -float("inf")
+            if nums[ind] < prevEl:
+                take = 1 + recursion(ind - 1, nums[ind])
+            return max(take, notTake)
+
+        # return recursion(len(nums)-1, float('inf'))
+
