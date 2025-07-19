88fe1d18b2e1ca247f43001798e5f935f0866de0
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-19 19:05:47 +0000

    [LeetCode Sync] Runtime - 12 ms (86.55%), Memory - 18.7 MB (100.00%)

--- my-folder/0055-jump-game/solution.py
+++ my-folder/0055-jump-game/solution.py
@@ -1,28 +1,61 @@
-class Solution(object):
-    def canJump(self, nums):
-        goal = len(nums)-1
-        for i in range(len(nums)-1,-1,-1):
-            if((nums[i]+i)>=goal):
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        # Greedy solution
+        goal = len(nums) - 1
+
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= goal:
                 goal = i
-        if(goal == 0):
-            return(True)
-        else:
-            return(False)
-        
-        """reach = [False]*len(nums)
-        # reach[-1] = True
-        for i in range(len(nums)-1,-1,-1):
-            if(nums[i]+i >= len(nums)-1):
-                reach[i] = True
-            else:
-                for j in range(i,nums[i]+i+1):
-                    if(reach[j]):
-                        reach[i] = True
-                        break
-        return(reach[0])
-        """
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        
+        return True if goal == 0 else False
+
+        # #DP: Memoization - TLE
+        # dp = defaultdict(bool)
+        # def recursion(ind): 
+        #     if ind == len(nums)-1:
+        #         return True
+        #     if ind > len(nums)-1 or nums[ind] == 0:
+        #         return False
+
+        #     if ind in dp:
+        #         return dp[ind]
+
+        #     for i in range(1, nums[ind] + 1):
+        #         if recursion(ind + i):
+        #             dp[ind] = True
+        #             return dp[ind]
+        #     dp[ind] = False
+        #     return dp[ind]
+
+        # return recursion(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # GOOD, BAD, UNKNOWN = 1, 0, -1
+        # memo = [UNKNOWN] * len(nums)
+        # memo[-1] = GOOD
+        # for i in range(len(nums) - 2, -1, -1):
+        #     furthest_jump = min(i + nums[i], len(nums) - 1)
+        #     for j in range(i + 1, furthest_jump + 1):
+        #         if memo[j] == GOOD:
+        #             memo[i] = GOOD
+        #             break
+        # return memo[0] == GOOD
