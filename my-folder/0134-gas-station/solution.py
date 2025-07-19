d78baae441a62866a30c24b8f8815d76e535e0de
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-19 23:52:38 +0000

    [LeetCode Sync] Runtime - 15 ms (94.27%), Memory - 23.3 MB (100.00%)

--- my-folder/0134-gas-station/solution.py
+++ my-folder/0134-gas-station/solution.py
@@ -1,42 +1,13 @@
-class Solution(object):
-    def canCompleteCircuit(self, gas, cost):
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         if sum(gas) < sum(cost):
             return -1
-
+        
         total = 0
         res = 0
         for i in range(len(gas)):
             total += (gas[i] - cost[i])
-
             if total < 0:
                 total = 0
                 res = i + 1
         return res
-        # BRUTE FORCE - N^2
-        # for i in range(len(gas)):
-        #     j = i+1
-        #     currFuel = gas[i] - cost[i]
-        #     while j != i:
-        #         # Base Case
-        #         if currFuel < 0:
-        #             break
-
-        #         # circular logic
-        #         if j == len(gas):
-        #             j = 0
-        #             if j == i:
-        #                 break
-                    
-        #         currFuel = currFuel + gas[j] - cost[j]
-        #         j += 1
-
-        #     if currFuel >= 0:
-        #         return i 
-        # return -1
-
-        """
-        :type gas: List[int]
-        :type cost: List[int]
-        :rtype: int
-        """
-        
