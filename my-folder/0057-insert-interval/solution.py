31e08d015a898334eed6ab61ace2facbb791a1ff
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-20 04:39:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.5 MB (100.00%)

--- my-folder/0057-insert-interval/solution.py
+++ my-folder/0057-insert-interval/solution.py
@@ -1,20 +1,34 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        n = len(intervals)
-        i = 0
-        output = []
+        res = []
 
-        while i < n and intervals[i][1] < newInterval[0]:
-            output.append(intervals[i])
-            i += 1
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]: # Case 1: add new interval - no overlapping, and return res
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]: # Case 2: add interval and continue - no overlapping
+                res.append(intervals[i])
+            else: # Overlapping
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+            
+        res.append(newInterval)
+        return res
+
+        # n = len(intervals)
+        # i = 0
+        # output = []
+
+        # while i < n and intervals[i][1] < newInterval[0]:
+        #     output.append(intervals[i])
+        #     i += 1
         
-        while i < n and newInterval[1] >= intervals[i][0]:
-            newInterval[0] = min(newInterval[0], intervals[i][0])
-            newInterval[1] = max(newInterval[1], intervals[i][1])
-            i += 1
-        output.append(newInterval)
+        # while i < n and newInterval[1] >= intervals[i][0]:
+        #     newInterval[0] = min(newInterval[0], intervals[i][0])
+        #     newInterval[1] = max(newInterval[1], intervals[i][1])
+        #     i += 1
+        # output.append(newInterval)
 
-        while i < n:
-            output.append(intervals[i])
-            i += 1
-        return output
+        # while i < n:
+        #     output.append(intervals[i])
+        #     i += 1
+        # return output
