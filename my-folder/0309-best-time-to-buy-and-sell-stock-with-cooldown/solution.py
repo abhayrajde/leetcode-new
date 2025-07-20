a9dc70039389cbba83cfae3d042652efd9b23891
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-20 23:29:09 +0000

    [LeetCode Sync] Runtime - 3 ms (62.44%), Memory - 18.7 MB (100.00%)

--- my-folder/0309-best-time-to-buy-and-sell-stock-with-cooldown/solution.py
+++ my-folder/0309-best-time-to-buy-and-sell-stock-with-cooldown/solution.py
@@ -1,28 +1,37 @@
-class Solution(object):
-    def maxProfit(self, prices):
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        #DP Memoization - Top Down Approach
+        dp = [[-1]*2 for i in range(len(prices))]
 
-        def dpmem(ind,canBuy):
+        def dpmem(ind: int, buy: bool):
             # Base Cases
-            if(ind >= len(prices)):
+            if ind >= len(prices):
                 return 0
-            if(dp[ind][canBuy] != -1):
-                return dp[ind][canBuy]
-
-            if(canBuy):
-                buy = -prices[ind] + dpmem(ind+1, False)
-                notBuy = 0 + dpmem(ind+1, True)
-                dp[ind][canBuy] = max(buy, notBuy)
+            if dp[ind][buy] != -1:
+                return dp[ind][buy]
+            if buy:
+                purchase = -prices[ind] + dpmem(ind + 1, False)
+                notPurchase = dpmem(ind + 1, True)
+                dp[ind][buy] = max(purchase, notPurchase)
+                return dp[ind][buy]
             else:
-                sell = prices[ind] + dpmem(ind+2, True)
-                notSell = 0 + dpmem(ind+1, False)
-                dp[ind][canBuy] = max(sell, notSell)
-            
-            return dp[ind][canBuy]
-        #Driver's Code
-        dp = [[-1 for i in range(2)] for j in range(len(prices))]
-        return(dpmem(0,1))
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
+                sell = prices[ind] + dpmem(ind + 2, True)
+                notSell = dpmem(ind + 1, False)
+                dp[ind][buy] = max(sell, notSell)
+                return dp[ind][buy]
+        return dpmem(0,True)
         
+        # Recursion
+        def recursion(ind: int, buy: bool):
+            # Base Cases
+            if ind >= len(prices):
+                return 0
+            if buy:
+                purchase = -prices[ind] + recursion(ind + 1, False)
+                notPurchase = recursion(ind + 1, True)
+                return max(purchase, notPurchase)
+            else:
+                sell = prices[ind] + recursion(ind + 2, True)
+                notSell = recursion(ind + 1, False)
+                return max(sell, notSell)
+        # return recursion(0,True)
