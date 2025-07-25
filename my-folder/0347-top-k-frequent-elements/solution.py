d6c388efb31e630aaf35eee3c368ffdc3e54116d
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-25 05:30:25 +0000

    [LeetCode Sync] Runtime - 8 ms (32.30%), Memory - 24.2 MB (26.59%)

--- my-folder/0347-top-k-frequent-elements/solution.py
+++ my-folder/0347-top-k-frequent-elements/solution.py
@@ -1,18 +1,30 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        hm = defaultdict(int)
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # TC: N - USING BUCKET SORT
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+        
         res = []
-        for i in range(len(nums)):
-            hm[nums[i]] += 1
-        temp = sorted(hm.items(), key = lambda x:x[1], reverse = True)
+        for i in range(len(freq)-1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+        # TC: NLOGM -> M = DICTIONARY LEN
+        res = []
+        hm = {}
+
+        for i in nums:
+            hm[i] = 1 + hm.get(i, 0)
+        
+        temp = sorted(hm.items(), key = lambda x: x[1], reverse = True)
         for i in range(k):
             res.append(temp[i][0])
         return res
 
-        
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        
+
