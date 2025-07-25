dbacf6e965750ddc3c989f84a64a964c70564a70
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-25 00:50:05 +0000

    [LeetCode Sync] Runtime - 18 ms (24.53%), Memory - 22.6 MB (39.96%)

--- my-folder/0049-group-anagrams/solution.py
+++ my-folder/0049-group-anagrams/solution.py
@@ -1,13 +1,22 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        hm = defaultdict(list)
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # OPTION 1
+        # TC: O(N * WLOG(W)) - W is len of word
+        # groups = defaultdict(list)
 
-        for i in range(len(strs)):
-            temp = "".join(sorted(strs[i]))
-            hm[temp].append(strs[i])
-        return (hm.values())
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        
+        # for s in strs:
+        #     temp = ''.join(sorted(s))
+        #     groups[temp].append(s)
+        # return list(groups.values())
+
+        # OPTION 2
+        # TC: O(N*W) - n = total words, w = avg len of the word
+        groups = defaultdict(list)
+
+        for s in strs:
+
+            count = [0] * 26    # as we know there will be only 26 characters lowercase(a-z)
+            for c in s:
+                count[ord(c) - ord('a')] += 1 #geting ascii value of the character
+            groups[tuple(count)].append(s)
+        return list(groups.values())
