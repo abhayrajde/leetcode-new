07a057c83ed9190866c7b99a7cbe2eafcf23dc5f
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-25 02:02:17 +0000

    [LeetCode Sync] Runtime - 363 ms (36.69%), Memory - 17.8 MB (100.00%)

--- my-folder/0005-longest-palindromic-substring/solution.py
+++ my-folder/0005-longest-palindromic-substring/solution.py
@@ -1,37 +1,22 @@
-class Solution(object):
-    def longestPalindrome(self, s):
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
         res = ""
-        resLen = [0]
-        # def updateLongestPalindrome(l,r):
-        #     while l >= 0 and r < len(s) and s[l] == s[r]:
-        #         if (r - l + 1) > resLen[0]:
-        #             res = s[l:r+1]
-        #             resLen[0] = len(res)
-        #         l -= 1
-        #         r += 1
         for i in range(len(s)):
-            # updateLongestPalindrome(i,i)
-            # updateLongestPalindrome(i,i+1)
-            l, r = i,i
+            # consider odd character strings
+            l, r = i, i
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > resLen[0]:
+                if r - l + 1 > len(res):
                     res = s[l:r+1]
-                    resLen[0] = len(res)
                 l -= 1
                 r += 1
-
-            l, r = i,i+1
+            
+            # considering even character strings
+            l, r = i, i + 1
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > resLen[0]:
+                if r - l + 1 > len(res):
                     res = s[l:r+1]
-                    resLen[0] = len(res)
                 l -= 1
                 r += 1
         return res
-            
 
-        """
-        :type s: str
-        :rtype: str
-        """
-        
+
