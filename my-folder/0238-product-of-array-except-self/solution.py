3df201254554bd7b01934de9a920f2e1cb57f0c4
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-25 05:07:50 +0000

    [LeetCode Sync] Runtime - 15 ms (94.24%), Memory - 23.2 MB (100.00%)

--- my-folder/0238-product-of-array-except-self/solution.py
+++ my-folder/0238-product-of-array-except-self/solution.py
@@ -1,20 +1,14 @@
-class Solution(object):
-    def productExceptSelf(self, nums):
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
         res = [1] * len(nums)
 
-        prefix = 1
+        prefix = 1 
         for i in range(len(nums)):
             res[i] = prefix
             prefix *= nums[i]
-
-        postfix = 1
-        for i in range(len(nums)-1,-1,-1):
-            res[i] *= postfix
-            postfix *= nums[i] 
-
-        return res
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
         
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
