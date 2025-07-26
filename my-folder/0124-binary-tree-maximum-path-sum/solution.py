2298a8b550173939495178abf7f46733d6ae3391
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-26 04:14:09 +0000

    [LeetCode Sync] Runtime - 11 ms (39.26%), Memory - 23 MB (99.97%)

--- my-folder/0124-binary-tree-maximum-path-sum/solution.py
+++ my-folder/0124-binary-tree-maximum-path-sum/solution.py
@@ -7,19 +7,17 @@
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         self.res = root.val
-
         def dfs(node):
             if not node:
                 return 0
             
             leftMax = dfs(node.left)
             rightMax = dfs(node.right)
+            
             leftMax = max(leftMax, 0)
             rightMax = max(rightMax, 0)
-
+            
             self.res = max(self.res, node.val + leftMax + rightMax)
-
             return node.val + max(leftMax, rightMax)
         dfs(root)
         return self.res
-        
