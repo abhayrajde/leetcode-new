3ea32b0e14fce597fd9aa8b699aaf395bbb22111
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-26 03:40:49 +0000

    [LeetCode Sync] Runtime - 3 ms (33.03%), Memory - 18.1 MB (100.00%)

--- my-folder/0046-permutations/solution.py
+++ my-folder/0046-permutations/solution.py
@@ -2,19 +2,21 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
         isTaken = [False] * len(nums)
-
-        def dfs(curr, isTaken):
-            if len(curr) == len(nums):
-                res.append(curr[:])
-                return
+        def recursion(currPath, isTaken):
+            if len(currPath) == len(nums):
+                res.append(currPath[:])
             
             for i in range(len(nums)):
                 if not isTaken[i]:
                     isTaken[i] = True
-                    curr.append(nums[i])
-                    dfs(curr, isTaken)
-                    curr.pop()
+                    currPath.append(nums[i])
+
+                    recursion(currPath, isTaken)
+                    #backtrack
                     isTaken[i] = False
-            
-        dfs([],isTaken)
+                    currPath.pop()
+        recursion([], isTaken)
         return res
+
+
+                
