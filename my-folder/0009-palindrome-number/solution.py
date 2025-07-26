23ff46bbdcb9a2f6eedfe8e7d501fa335ed8f615
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-26 22:06:33 +0000

    [LeetCode Sync] Runtime - 4 ms (80.59%), Memory - 18 MB (100.00%)

--- my-folder/0009-palindrome-number/solution.py
+++ my-folder/0009-palindrome-number/solution.py
@@ -1,13 +1,17 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        temp1 = str(x)
-        temp2 = temp1[-1::-1]
-        if(temp1 == temp2):
-            return(True)
-        else:
-            return(False)
-        """
-        :type x: int
-        :rtype: bool
-        """
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0: return False
+        l1 = []
+        while x >= 1:
+            rem = x % 10
+            l1.append(rem)
+            x = x // 10
         
+        l, r = 0, len(l1)-1
+        while l <= r:
+            if l1[l] != l1[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+
