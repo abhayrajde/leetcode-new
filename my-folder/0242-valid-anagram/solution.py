68f5f9f41b51d28819289b6360dbab2ac5b67bfb
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-26 18:27:23 +0000

    [LeetCode Sync] Runtime - 14 ms (46.47%), Memory - 17.7 MB (100.00%)

--- my-folder/0242-valid-anagram/solution.py
+++ my-folder/0242-valid-anagram/solution.py
@@ -1,24 +1,16 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        # base case
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
+        
+        countS, countT = {}, {}
 
-        hm1 = defaultdict(int)
-        hm2 = defaultdict(int)
-        
         for i in range(len(s)):
-            hm1[s[i]] += 1
-            hm2[t[i]] += 1
-        
-        for i in hm1.keys():
-            if hm1[i] != hm2[i]:
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
                 return False
         return True
 
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        
