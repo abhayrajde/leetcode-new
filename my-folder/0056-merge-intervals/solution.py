ccaa7655b2547aa85f5832ee1619e6e158e1173f
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-27 21:25:02 +0000

    [LeetCode Sync] Runtime - 7 ms (71.90%), Memory - 20.9 MB (100.00%)

--- my-folder/0056-merge-intervals/solution.py
+++ my-folder/0056-merge-intervals/solution.py
@@ -4,16 +4,16 @@ class Solution:
         # from index 1 to end, 
         # if current's start <= prev's end, prev[1] = max(curr's last, prev's last)
         # append prev at the very end
-        output = []
-        intervals.sort(key=lambda x: x[0])
-
+        res = []
+        intervals.sort()
         prev = intervals[0]
         for i in range(1, len(intervals)):
-            interval = intervals[i]
-            if interval[0] <= prev[1]:
-                prev[1] = max(prev[1], interval[1])
+            if intervals[i][0] <= prev[1]:
+                prev[1] = max(prev[1], intervals[i][1])
             else:
-                output.append(prev)
-                prev = interval
-        output.append(prev)
-        return output
+                res.append(prev)
+                prev = intervals[i]
+        res.append(prev)
+        return res
+
+
