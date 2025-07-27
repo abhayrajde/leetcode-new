58e4b1ba5aeb73f34709d513bad07c097949aafd
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-27 04:22:53 +0000

    [LeetCode Sync] Runtime - 3 ms (76.52%), Memory - 17.7 MB (100.00%)

--- my-folder/0036-valid-sudoku/solution.py
+++ my-folder/0036-valid-sudoku/solution.py
@@ -1,24 +1,18 @@
-class Solution(object):
-    def isValidSudoku(self, board):
-        cols = defaultdict(set)
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
         rows = defaultdict(set)
+        cols = defaultdict(set)
         squares = defaultdict(set)
 
-        for r in range (9):
-            for c in range (9):
-                if board[r][c] == ".":
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if board[r][c] == '.':
                     continue
-                if (board[r][c] in rows[r] or
-                    board[r][c] in cols[c] or
-                    board[r][c] in squares[(r // 3, c // 3)]):
+                
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r // 3, c // 3)]:
                     return False
+                
                 rows[r].add(board[r][c])
                 cols[c].add(board[r][c])
-                squares[(r // 3, c // 3)].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
         return True
-
-        """
-        :type board: List[List[str]]
-        :rtype: bool
-        """
-        
