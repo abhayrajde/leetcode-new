e1e64d7ee5e441591eee29fa1d15f435a02b2249
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-07-28 22:37:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.2 MB (100.00%)

--- my-folder/0091-decode-ways/solution.py
+++ my-folder/0091-decode-ways/solution.py
@@ -1,5 +1,46 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        # DP - MEMOIZATION
+        dp = [-1] * len(s)
+        def dpmem(ind):
+            if ind == len(s):
+                return 1
+            if ind > len(s):
+                return 0
+            if dp[ind] != -1:
+                return dp[ind]
+            
+            opt1 = opt2 = 0
+            if s[ind] != '0':
+                opt1 = dpmem(ind+1)
+
+            if s[ind] != '0' and int(s[ind:ind+2]) <= 26:
+                opt2 = dpmem(ind + 2)
+            dp[ind] = opt1 + opt2
+            return dp[ind]
+        return dpmem(0)
+        
+        #Recursion
+        def recursion(ind):
+            if ind == len(s):
+                return 1
+            if ind > len(s):
+                return 0
+            
+            opt1 = opt2 = 0
+            if s[ind] != '0':
+                opt1 = recursion(ind+1)
+
+            if s[ind] != '0' and int(s[ind:ind+2]) <= 26:
+                opt2 = recursion(ind + 2)
+            return opt1 + opt2
+        return recursion(0)
+
+
+
+
+
+
         #this is only bottum up
         @lru_cache(maxsize=None)
         def traverse(i):
