6053451b309eadf47ae29a68f44ab1aa1a5b4e5a
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-08-02 20:33:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 49.7 MB (11.12%)

--- my-folder/0111-minimum-depth-of-binary-tree/solution.py
+++ my-folder/0111-minimum-depth-of-binary-tree/solution.py
@@ -1,29 +1,23 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def minDepth(self, root):
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        count = 0
         q = deque([root])
-        while q:
-            count += 1
-            for i in range(len(q)):
-                curr = q.popleft()
-                if(not curr.left and not curr.right):
-                    return count
-                if(curr.left):
-                    q.append(curr.left)
-                if(curr.right):
-                    q.append(curr.right)
-        return count
-
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
+        counter = 1
         
+        while q:
+            for _ in range(len(q)):
+                curr = q.popleft()
+                if not curr.left and not curr.right:
+                    return counter
+                if curr.left:
+                    q.append(curr.left)
+                if curr.right:
+                    q.append(curr.right)
+            counter += 1
