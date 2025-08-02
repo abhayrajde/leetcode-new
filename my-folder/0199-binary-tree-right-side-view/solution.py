a94b0d5679e03922de736d290b426f6ffbc4a821
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-08-02 21:59:00 +0000

    [LeetCode Sync] Runtime - 2 ms (10.69%), Memory - 17.9 MB (100.00%)

--- my-folder/0199-binary-tree-right-side-view/solution.py
+++ my-folder/0199-binary-tree-right-side-view/solution.py
@@ -1,53 +1,26 @@
-# # Definition for a binary tree node.
-# # class TreeNode:
-# #     def __init__(self, val=0, left=None, right=None):
-# #         self.val = val
-# #         self.left = left
-# #         self.right = right
-# class Solution:
-#     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-#         # Base Case
-#         if not root:
-#             return []
-        
-#         res = []
-#         q = deque([root])
-#         while q:
-#             qLen = len(q)
-#             for i in range(qLen):
-#                 node = q.popleft()
-
-#                 if i == qLen - 1:
-#                     res.append(node.val)
-                
-#                 if node.left:
-#                     q.append(node.left)
-#                 if node.right:
-#                     q.append(node.right)
-#         return res
-        
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        res = []
+        if not root:
+            return []
         q = deque([root])
 
+        res = []
         while q:
-            rightSide = None
             qLen = len(q)
-
             for i in range(qLen):
-                node = q.popleft()
-                if node:
-                    rightSide = node
-                    q.append(node.left)
-                    q.append(node.right)
-            if rightSide:
-                res.append(rightSide.val)
+                curr = q.popleft()
+                if curr.left:
+                    q.append(curr.left)
+                if curr.right:
+                    q.append(curr.right)
+                
+                if i == qLen - 1:
+                    res.append(curr.val)
         return res
+
