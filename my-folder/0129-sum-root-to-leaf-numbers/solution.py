ea498affa3f05c9db04ad8ba873ee92d13dd2cfe
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-08-02 22:39:04 +0000

    [LeetCode Sync] Runtime - 4 ms (1.70%), Memory - 17.8 MB (100.00%)

--- my-folder/0129-sum-root-to-leaf-numbers/solution.py
+++ my-folder/0129-sum-root-to-leaf-numbers/solution.py
@@ -1,25 +1,19 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def sumNumbers(self, root):
-        res = [0]
-        def dfs(node, s):
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        self.res = 0
+        def dfs(node, currNum):
             if not node:
-                return
+               return
             if not node.left and not node.right:
-                res[0] += int(s+str(node.val))
-            dfs(node.left, s+str(node.val))
-            dfs(node.right, s+str(node.val))
-        dfs(root,"")
-        return res[0]
-            
-
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        
+                self.res += int(currNum + str(node.val))
+            newNum = currNum + str(node.val)
+            left = dfs(node.left, newNum) 
+            right = dfs(node.right, newNum)
+        dfs(root, '')
+        return self.res
