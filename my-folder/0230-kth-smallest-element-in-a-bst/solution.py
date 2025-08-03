b8305e7e109a01580c38edf9f5152298d2130805
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-08-03 15:04:10 +0000

    [LeetCode Sync] Runtime - 4 ms (14.08%), Memory - 21.1 MB (99.92%)

--- my-folder/0230-kth-smallest-element-in-a-bst/solution.py
+++ my-folder/0230-kth-smallest-element-in-a-bst/solution.py
@@ -7,15 +7,13 @@
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         arr = []
-    
+
         def dfs(node):
             if not node:
-                return 
-            dfs(node.left)
-            arr.append(node.val)
-            dfs(node.right)
-        dfs(root)
-        return arr[k - 1]
+                return
             
-
-        
+            left = dfs(node.left)
+            arr.append(node.val)
+            right = dfs(node.right)
+        dfs(root)
+        return arr[k-1]
