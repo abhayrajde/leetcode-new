2450f90916503897834e140d920bf65057503df2
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-08-03 05:07:35 +0000

    [LeetCode Sync] Runtime - 918 ms (18.25%), Memory - 34.7 MB (81.51%)

--- my-folder/1250-longest-common-subsequence/solution.py
+++ my-folder/1250-longest-common-subsequence/solution.py
@@ -1,43 +1,32 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # DP: Tabulation
-        dp = [[0]* (len(text2)+1) for i in range(len(text1) + 1)]
-        for i1 in range(len(text1)-1, -1, -1):
-            for i2 in range(len(text2)-1, -1, -1):
-                if text1[i1] == text2[i2]:
-                    dp[i1][i2] = 1 + dp[i1 + 1][i2 + 1]
-                else:
-                    dp[i1][i2] = max(dp[i1][i2 + 1], dp[i1 + 1][i2])
-        return dp[0][0]
-
-
-        
-        # DP: Memoization - Top Down approach
-        # TC - (N*M) | SC - O(N * M) + O(N + M)
-        # O(N + M) - is an auxilary space - watch video for it.
-        dp = [[-1]*len(text2) for i in range(len(text1))]
+        # DP: MEMOIZATION
+        dp = [[-1] * len(text2) for _ in range(len(text1))]
         def dpmem(i1, i2):
-            # Base Case
-            if i1 < 0 or i2 < 0:
+            if i1 == len(text1) or i2 == len(text2):
                 return 0
+            
             if dp[i1][i2] != -1:
                 return dp[i1][i2]
 
             if text1[i1] == text2[i2]:
-                dp[i1][i2] = 1 + dpmem(i1 - 1, i2 - 1)
+                dp[i1][i2] = 1 + dpmem(i1 + 1, i2 + 1)
                 return dp[i1][i2]
-            
-            dp[i1][i2] = max(dpmem(i1 - 1, i2), dpmem(i1, i2 - 1))
+            opt1 = dpmem(i1, i2 + 1)
+            opt2 = dpmem(i1 + 1, i2)
+            dp[i1][i2] = max(opt1, opt2)
             return dp[i1][i2]
+        return dpmem(0,0)
 
-        return dpmem(len(text1)-1, len(text2)-1)
-        # def recursion(i1, i2):
-        #     # Base Case
-        #     if i1 < 0 or i2 < 0:
-        #         return 0
+        # Recursion - Brute Force
+        def recursion(i1, i2):
+            if i1 == len(text1) or i2 == len(text2):
+                return 0
 
-        #     if text1[i1] == text2[i2]:
-        #         return 1 + recursion(i1 - 1, i2 - 1)
-        #     return max(recursion(i1 - 1, i2), recursion(i1, i2 - 1))
-        # return recursion(len(text1)-1, len(text2)-1)
+            if text1[i1] == text2[i2]:
+                return 1 + recursion(i1 + 1, i2 + 1)
+            opt1 = recursion(i1, i2 + 1)
+            opt2 = recursion(i1 + 1, i2)
+            return max(opt1,opt2)
+        return recursion(0,0)
 
