58b7d1ea412208182c2dc403eb201772d294fe6a
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-08-03 02:36:18 +0000

    [LeetCode Sync] Runtime - 159 ms (19.72%), Memory - 36.4 MB (25.62%)

--- my-folder/0084-largest-rectangle-in-histogram/solution.py
+++ my-folder/0084-largest-rectangle-in-histogram/solution.py
@@ -1,16 +1,23 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
+        # Monotonic stack - increasing monotonic
+        stack = [] # pair: (index, height)
         maxArea = 0
-        stack = []
 
+        # 3 Possiblities:
+        # 1 - prev element > curr element
+        # 2 - prev element < curr element
+        # 3 - prev element = curr element
         for i, h in enumerate(heights):
             start = i
-            while stack and stack[-1][1] > h:
+            while stack and stack[-1][1] > h: # while scenario 1 is True
                 index, height = stack.pop()
                 maxArea = max(maxArea, height * (i - index))
                 start = index
             stack.append((start, h))
-
+        
         for i, h in stack:
             maxArea = max(maxArea, h * (len(heights) - i))
         return maxArea
+
+
