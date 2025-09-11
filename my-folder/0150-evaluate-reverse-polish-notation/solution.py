557e8333be04853b172de8f27e93396380a65841
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-09-11 22:41:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.2 MB (100.00%)

--- my-folder/0150-evaluate-reverse-polish-notation/solution.py
+++ my-folder/0150-evaluate-reverse-polish-notation/solution.py
@@ -1,52 +1,17 @@
 class Solution:
-    # @param {string[]} tokens
-    # @return {integer}
-    def __init__(self):
-        self.operators = {
-            '+': lambda y, x: x + y,
-            '-': lambda y, x: x - y,
-            '*': lambda y, x: x * y,
-            '/': lambda y, x: int(operator.truediv(x, y))
-        }
-
-    def evalRPN(self, tokens):
-        if not tokens:
-            return 0
-
+    def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-
-        for token in tokens:
-            if token in self.operators:
-                stack.append(self.operators[token](stack.pop(), stack.pop()))
+        for element in tokens:
+            if element == '+':
+                stack.append(stack.pop() + stack.pop())
+            elif element == '-':
+                secondEl, firstEl = stack.pop(), stack.pop()
+                stack.append(firstEl - secondEl)
+            elif element == '*':
+                stack.append(stack.pop() * stack.pop())
+            elif element == '/':
+                secondEl, firstEl = stack.pop(), stack.pop()
+                stack.append(int(firstEl / secondEl))
             else:
-                stack.append(int(token))
-
+                stack.append(int(element))
         return stack[0]
-# class Solution(object):
-#     def evalRPN(self, tokens):
-#         stack = []
-#         for i in tokens:
-#             if i == '+':
-#                 d2 = stack.pop()
-#                 d1 = stack.pop()
-#                 stack.append(d1 + d2)
-#             elif i == '-':
-#                 d2 = stack.pop()
-#                 d1 = stack.pop()
-#                 stack.append(d1 - d2)
-#             elif i == '*':
-#                 d2 = stack.pop()
-#                 d1 = stack.pop()
-#                 stack.append(d1 * d2)
-#             elif i == '/':
-#                 d2 = stack.pop()
-#                 d1 = stack.pop()
-#                 stack.append(int(d1 / d2))
-#             else:
-#                 stack.append(int(i))
-#         return stack[0]
-#         """
-#         :type tokens: List[str]
-#         :rtype: int
-#         """
-        
