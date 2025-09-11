5c6bc7f75054adc3ca9c211adf79833f66dfaab2
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-09-11 02:14:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.9 MB (100.00%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,7 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hm = collections.defaultdict(int)
-
+        hm = {}
         for i in range(len(nums)):
             difference = target - nums[i]
             if difference in hm:
