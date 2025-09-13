d699e5e49cdc498d50e84e8748dfe1cb3f2e87cd
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-09-13 06:23:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (100.00%)

--- my-folder/0226-invert-binary-tree/solution.py
+++ my-folder/0226-invert-binary-tree/solution.py
@@ -6,7 +6,6 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-
         def dfs(node):
             if not node:
                 return
