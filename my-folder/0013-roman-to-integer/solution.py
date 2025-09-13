f9e41b5ca1023d965e242223d327ebe16213d05b
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-09-13 21:30:46 +0000

    [LeetCode Sync] Runtime - 4 ms (64.33%), Memory - 17.6 MB (100.00%)

--- my-folder/0013-roman-to-integer/solution.py
+++ my-folder/0013-roman-to-integer/solution.py
@@ -1,13 +1,11 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         rome_dict = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
-
         res = 0
-        for i in range(len(s)-1):
+        for i in range(len(s) - 1):
             if rome_dict[s[i]] < rome_dict[s[i+1]]:
                 res -= rome_dict[s[i]]
             else:
                 res += rome_dict[s[i]]
         res += rome_dict[s[-1]]
         return res
-
