39595b34648b9f0130940834c15f02b6c8d04070
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-09-14 22:22:37 +0000

    [LeetCode Sync] Runtime - 2166 ms (5.00%), Memory - 482.2 MB (5.00%)

--- my-folder/0322-coin-change/solution.py
+++ my-folder/0322-coin-change/solution.py
@@ -1,5 +1,23 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = {}
+        def dpmem(ind, target):
+            if target == 0: return 0
+            if ind == len(coins) or target < 0: return float('inf')
+
+            if (ind, target) in dp: return dp[(ind, target)]
+
+            pick = 1 + dpmem(ind, target - coins[ind])
+            notPick = dpmem(ind + 1, target)
+
+            dp[(ind, target)] = min(pick, notPick)
+            return dp[(ind, target)]
+            
+        totCoins = dpmem(0, amount)
+        return totCoins if totCoins != float('inf') else -1
+
+
+
         # DP: memoization 
         dp = [[-1] * (amount + 1) for i in range(len(coins))]
         def dpmem(ind, target): # returns -> number of coins  
