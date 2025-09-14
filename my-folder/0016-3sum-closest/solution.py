300a7db54a63186d25731b37e920f83b08454d8b
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-09-14 04:55:16 +0000

    [LeetCode Sync] Runtime - 344 ms (82.73%), Memory - 17.8 MB (100.00%)

--- my-folder/0016-3sum-closest/solution.py
+++ my-folder/0016-3sum-closest/solution.py
@@ -1,57 +1,24 @@
-class Solution(object):
-    def threeSumClosest(self, nums, target):
-        """
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        res = float('inf')
         nums.sort()
-        min1 = 100000000000
-        for i in range(len(nums)-2):
-            low = i+1
-            high = len(nums)-1
-            while(low<high):
-                temp = nums[i]+nums[low]+nums[high]
-                if(temp>target):
-                    temp1 = temp-target
-                    min1 = min(min1,temp1)
-                    if(min1 == temp1):
-                        mintemp = temp
-                    high-=1
+
+        for i in range(len(nums)):
+
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                currSum = nums[i] + nums[l] + nums[r]
+
+                if abs(currSum - target) < abs(res - target):
+                    res = currSum
+
+                if currSum > target:
+                    r -= 1
                     
-                elif(temp<target):
-                    temp1 = target-temp
-                    min1 = min(min1,temp1)
-                    if(min1 == temp1):
-                        mintemp = temp
-                    low+=1
-                    
+                elif currSum < target:
+                    l += 1
                 else:
-                    return(target)
-        return(mintemp)
-        
-        
-        """
-        #FOUND THIS SOLUTION IN DISCUSSION, WHICH IS IS KINDOFF EASIER AND MORE APPROACHABLE
-        #GETTING THE ABSOLUTE VALUE IN BOTH THE WAY MAKES THIS SOLUTION MORE APPROACHABLE AND EASY
-        nums.sort()
-        result = nums[0] + nums[1] + nums[2]
-        for i in range(len(nums) - 2):
-            j, k = i+1, len(nums) - 1
-            while j < k:
-                sum = nums[i] + nums[j] + nums[k]
-                if sum == target:
-                    return sum
-                
-                if abs(sum - target) < abs(result - target):
-                    result = sum
-                
-                if sum < target:
-                    j += 1
-                elif sum > target:
-                    k -= 1
-            
-        return result
-        """ """       
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        
+                    return target
+        return res
+
+
