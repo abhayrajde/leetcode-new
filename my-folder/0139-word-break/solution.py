0230cff8ef208a57e54a5a68d7307df8c2c88008
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-09-15 01:37:22 +0000

    [LeetCode Sync] Runtime - 3 ms (74.06%), Memory - 17.9 MB (100.00%)

--- my-folder/0139-word-break/solution.py
+++ my-folder/0139-word-break/solution.py
@@ -1,5 +1,43 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # Dp - Tabulation
+        dp = [False] * (len(s)+1)
+        dp[-1] = True
+
+        for i in range(len(s)-1, -1, -1):
+            for w in wordDict:
+                wl = len(w)
+                if i + wl <= len(s) and s[i : i + wl] == w:
+                    dp[i] = dp[i + wl]
+
+                    if dp[i]:
+                        break
+        return dp[0]
+
+
+        # DP - Memoization
+        dp = {}
+
+        def dpmem(ind):
+            if ind >= len(s):
+                return True
+            
+            if ind in dp:
+                return dp[ind]
+
+            for w in wordDict:
+                wl = len(w)
+                if ind + wl <= len(s) and s[ind : ind + wl] == w:
+                    if dpmem(ind + wl):
+                        dp[ind] = True
+                        return dp[ind]
+            dp[ind] = False                    
+            return dp[ind]
+        return dpmem(0)
+
+
+
+
         # DP - TABULATION - OPTIMIZED
         dp = [False] * (len(s) + 1)
         dp[len(s)] = True
