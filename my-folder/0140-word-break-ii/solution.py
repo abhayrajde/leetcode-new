d51da87e5007f997f37f56705b5c389992446332
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-09-15 00:49:16 +0000

    [LeetCode Sync] Runtime - 3 ms (5.52%), Memory - 17.9 MB (100.00%)

--- my-folder/0140-word-break-ii/solution.py
+++ my-folder/0140-word-break-ii/solution.py
@@ -1,38 +1,18 @@
-class Solution(object):
-    def wordBreak(self, s, wordDict):
-        
-        # We will be using memoization technique in dp
-        dp = {}
-        
-        def dfs(string):
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        res = []
+        def dpmem(ind, currPath):
+            if ind >= len(s):
+                temp = ''
+                for i in range(len(currPath)-1):
+                    temp = temp + currPath[i] + ' '
+                temp += currPath[-1]
+                res.append(temp)
             
-            #if the string is available in dp dic which might have been encountered before than return that
-            if string in dp:
-                return dp[string]
-            
-            #if the string is empty
-            if not string:
-                return [""]
-            
-            local_res = []
-            
-            for word in wordDict:
-                if string.startswith(word):
-                    sub_words = dfs(string[len(word):])
-                    print(sub_words)
-                    
-                    for sub_word in sub_words:
-                        
-                        local_res.append(word + (" " if sub_word else "") + sub_word)
-                        
-            dp[string] = local_res
-            
-            return local_res
-        return dfs(s)
-            
-        """
-        :type s: str
-        :type wordDict: List[str]
-        :rtype: List[str]
-        """
-        
+            for w in wordDict:
+                if ind + len(w) <= len(s) and s[ind: ind + len(w)] == w:
+                    dpmem(ind + len(w), currPath + [w])
+        dpmem(0,[])
+
+        return res
+                
