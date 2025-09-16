b5f1df9d7cbfa79970fd47dd1445a93193717f70
Abhay Rajde <abhaythakkar1234@gmail.com>
2025-09-16 01:12:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.8 MB (100.00%)

---
+++ my-folder/0171-excel-sheet-column-number/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        res = 0
+        for c in columnTitle:
+            res *= 26
+            res += (ord(c) - ord('A') + 1)
+        return res
+
