ee0b817ffa64e86b650a3fa5ef3409ffb1b6c9d0
Abhay Rajde <abhaythakkar1234@gmail.com>
2026-05-03 17:18:06 +0000

    [LeetCode Sync] Runtime - 839 ms (5.00%), Memory - 77.4 MB (5.00%)

--- my-folder/0125-valid-palindrome/solution.cs
+++ my-folder/0125-valid-palindrome/solution.cs
@@ -1,10 +1,12 @@
 public class Solution {
     public bool IsPalindrome(string s) {
-        string cleanString = new string(s.Where(char.IsLetterOrDigit)
-                                        .Select(char.ToLower)
-                                        .ToArray());
-        
-        Console.WriteLine(cleanString);
-        return cleanString == new string(cleanString.Reverse().ToArray());
+        string result = "";
+        foreach(char c in s) {
+            if (char.IsLetterOrDigit(c)) {
+                result += char.ToLower(c);
+            }
+        }
+
+        return (result == new string(result.Reverse().ToArray()));
     }
 }
