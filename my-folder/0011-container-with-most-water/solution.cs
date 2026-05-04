2b1caa9cef50212d65814178d946c85730a80296
Abhay Rajde <abhaythakkar1234@gmail.com>
2026-05-04 12:38:21 +0000

    [LeetCode Sync] Runtime - 1 ms (99.74%), Memory - 61.4 MB (79.05%)

---
+++ my-folder/0011-container-with-most-water/solution.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int MaxArea(int[] height) {
+        int res = 0;
+        int l = 0, r = height.Length - 1;
+        while (l < r) {
+            int area = Math.Min(height[l], height[r]) * (r - l);
+            res = Math.Max(res, area);
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return res;
+    }
+}
