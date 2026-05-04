c394c90c1c1012e78a006dcee0f93e486d281359
Abhay Rajde <abhaythakkar1234@gmail.com>
2026-05-04 13:51:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 46.7 MB (56.55%)

---
+++ my-folder/0042-trapping-rain-water/solution.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int Trap(int[] height) {
+        int l = 0, r = height.Length - 1;
+        int leftMax = height[l], rightMax = height[r];
+        int water = 0;
+
+        while(l < r) {
+            if (leftMax < rightMax) {
+                l++;
+                leftMax = Math.Max(leftMax, height[l]);
+                water += leftMax - height[l];
+            } else {
+                r--;
+                rightMax = Math.Max(rightMax, height[r]);
+                water += rightMax - height[r];
+            }
+        }
+        return water;
+    }
+}
