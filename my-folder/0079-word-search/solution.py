303d28793d9f28b56c1efe37ed82322d14d0703d
Abhay Rajde <abhaythakkar1234@gmail.com>
2026-05-15 19:16:48 +0000

    [LeetCode Sync] Runtime - 4195 ms (40.13%), Memory - 19.4 MB (82.93%)

--- my-folder/0079-word-search/solution.py
+++ my-folder/0079-word-search/solution.py
@@ -2,30 +2,32 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         ROWS = len(board)
         COLS = len(board[0])
-
         visited = set()
-
-        def dfs(r, c, i):
+        
+        def dfs(r, c, ind):
             # Base Cases
-            if i == len(word):
+            if ind >= len(word):
                 return True
-            
-            if r < 0 or r >= ROWS or c < 0 or c>= COLS or (r,c) in visited or board[r][c] != word[i]:
+
+            if r < 0 or r >= ROWS or c < 0 or c >= COLS or word[ind] != board[r][c] or (r,c) in visited:
                 return False
 
             visited.add((r,c))
-            res = (dfs(r + 1, c, i + 1) or
-            dfs(r - 1, c, i + 1) or
-            dfs(r, c + 1, i + 1) or
-            dfs(r, c - 1, i + 1))
-            
+
+            res = (dfs(r+1,c,ind+1) or
+            dfs(r-1,c,ind+1) or
+            dfs(r,c+1,ind+1) or
+            dfs(r,c-1,ind+1))
+
             visited.remove((r,c))
             return res
-        
+
         for r in range(ROWS):
             for c in range(COLS):
-                if dfs(r, c, 0): return True
+                if board[r][c] == word[0]:
+                    if dfs(r,c,0):
+                        return True
         return False
-            
 
-            
+        
+
