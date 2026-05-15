a53159eadb4a1a74e35f88ad1e54d22f177dd2e3
Abhay Rajde <abhaythakkar1234@gmail.com>
2026-05-15 09:25:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (31.54%)

--- my-folder/0100-same-tree/solution.py
+++ my-folder/0100-same-tree/solution.py
@@ -6,12 +6,16 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
-            return True
-        
-        if not p or not q or p.val != q.val:
-            return False 
-        
-        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right, q.right) 
+        def dfs(node1, node2):
+            if not node1 and not node2:
+                return True
 
-        
+            if not node1 or not node2 or node1.val != node2.val:
+                return False
+            
+            left = dfs(node1.left, node2.left)
+            right = dfs(node1.right, node2.right)
+            
+            return left and right
+        return dfs(p,q)
+
