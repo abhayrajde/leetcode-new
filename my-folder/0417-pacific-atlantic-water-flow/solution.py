c917c2911381e4047ca0c9c6d9bfc2630b0241da
Abhay Rajde <abhaythakkar1234@gmail.com>
2026-05-16 14:33:33 +0000

    [LeetCode Sync] Runtime - 28 ms (64.78%), Memory - 22.2 MB (38.35%)

--- my-folder/0417-pacific-atlantic-water-flow/solution.py
+++ my-folder/0417-pacific-atlantic-water-flow/solution.py
@@ -8,7 +8,7 @@ class Solution:
         res = []
 
         def dfs(r, c, visited, prevHeight):
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r,c) in visited or  heights[r][c] < prevHeight:
+            if r < 0 or r >= ROWS or c < 0 or c >= COLS or heights[r][c] < prevHeight or (r,c) in visited:
                 return
             
             visited.add((r,c))
@@ -16,19 +16,18 @@ class Solution:
             dfs(r - 1, c, visited, heights[r][c])
             dfs(r, c + 1, visited, heights[r][c])
             dfs(r, c - 1, visited, heights[r][c])
-
-        for c in range(COLS):
-            dfs(0, c, pac, heights[0][c])
-            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
         
+        for c in range(COLS):
+            dfs(0, c, pac, -float('inf'))
+            dfs(ROWS - 1, c, atl, -float('inf'))
+
         for r in range(ROWS):
-            dfs(r, 0, pac, heights[r][0])
-            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
+            dfs(r, 0, pac, -float('inf'))
+            dfs(r, COLS - 1, atl, -float('inf'))
         
         for r in range(ROWS):
             for c in range(COLS):
-                if (r,c) in pac and (r,c) in atl:
+                if (r,c) in atl and (r,c) in pac:
                     res.append([r,c])
         return res
 
-        
