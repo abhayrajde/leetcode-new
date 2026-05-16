40463b9cd52614a2ab0d87107bfb4c1d9a99cb1b
Abhay Rajde <abhaythakkar1234@gmail.com>
2026-05-16 16:26:11 +0000

    [LeetCode Sync] Runtime - 3 ms (42.23%), Memory - 21.3 MB (18.78%)

--- my-folder/0261-graph-valid-tree/solution.py
+++ my-folder/0261-graph-valid-tree/solution.py
@@ -1,26 +1,24 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if not n:
-            return True
-        
-        adj = {i:[] for i in range(n)}
+        graph = defaultdict(list)
 
-        for n1, n2 in edges:
-            adj[n1].append(n2)
-            adj[n2].append(n1)
+        for v1, v2 in edges:
+            graph[v1].append(v2)
+            graph[v2].append(v1)
 
-        visited = set()
-        def dfs(node, prev):
-            # Base case
-            if node in visited:
+        cycle = set() # elements in curr cycle
+        def dfs(curr, prev):
+            if curr in cycle:
                 return False
-
-            visited.add(node)
-            for nei in adj[node]:
+            
+            cycle.add(curr)
+            for nei in graph[curr]:
                 if nei == prev:
                     continue
-                if not dfs(nei, node):
+                if not dfs(nei, curr):
                     return False
             return True
-        return dfs(0, -1) and n == len(visited)
-             
+        
+        # Driver Code
+        return dfs(0, -1) and n == len(cycle)
+
