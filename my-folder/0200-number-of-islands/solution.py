da612ce0aa04731aa5dd6214f3cf9e09be45c0a7
Abhay Rajde <abhaythakkar1234@gmail.com>
2026-05-16 13:33:25 +0000

    [LeetCode Sync] Runtime - 257 ms (33.24%), Memory - 31 MB (11.00%)

--- my-folder/0200-number-of-islands/solution.py
+++ my-folder/0200-number-of-islands/solution.py
@@ -2,20 +2,21 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         ROWS = len(grid)
         COLS = len(grid[0])
-
         visited = set()
-        islands = 0
-
         def dfs(r, c):
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r,c) in visited or grid[r][c] == '0':
-                return
+            # Base Case
+            if r < 0 or r >= ROWS or c < 0 or c >= COLS or grid[r][c] == '0' or (r,c) in visited:
+                return False
 
             visited.add((r,c))
-            dfs(r+1,c)
-            dfs(r-1,c)
-            dfs(r,c+1)
-            dfs(r,c-1)
 
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+
+        islands = 0
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == '1' and (r,c) not in visited:
